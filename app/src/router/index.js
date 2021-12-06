a7f3cc58a99cd9f48fac137e98b66c0d03cb8c11
tholulomo <fatexjames@yahoo.com>
2021-12-05 19:48:43 -0500

    #6: Updated to created app shell

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,26 +1,41 @@
+import { defineAsyncComponent } from 'vue';
 import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
 
+import Home from "../pages/Home.vue";
+
+const AboutPage = defineAsyncComponent(() =>
+  import('../pages/About.vue')
+);
+
+// route level code-splitting
+// this generates a separate chunk (about.[hash].js) for this route
+// which is lazy-loaded when the route is visited.
 const routes = [
   {
     path: "/",
-    name: "Home",
     component: Home,
   },
   {
     path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: AboutPage,
+    meta: { requiresAuth: true }
   },
+  { path: '/:notFound(.*)', component: Home } //TODO: Not found component
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
   routes,
+});
+
+router.beforeEach(function(to, _, next) {
+  if (to.meta.requiresAuth && !store.getters.isAuthenticated) {
+    next('/'); //TODO: Develop auth component
+  } else if (to.meta.requiresUnauth && store.getters.isAuthenticated) {
+    next('/');
+  } else {
+    next();
+  }
 });
 
 export default router;
--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,8 +1,12 @@
 import { createStore } from "vuex";
 
+import authModule from './modules/auth/index.js';
+
 export default createStore({
   state: {},
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    auth: authModule
+  },
 });
