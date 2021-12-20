766e5e37ac5dbd34eacff6401c975ccbc2830b00
tholulomo <fatexjames@yahoo.com>
2021-12-19 20:32:26 -0500

    #33: Added vue material package

--- app/src/main.js
+++ app/src/main.js
@@ -1,10 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import VueMaterial from 'vue-material';
+import 'vue-material/dist/vue-material.min.css';
+import 'vue-material/dist/theme/default.css';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+Vue.use(VueMaterial);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
--- app/src/pages/Home.vue
+++ app/src/pages/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
+    <md-switch v-model="boolean">Accent <small>(Default)</small></md-switch>
+    <md-switch v-model="boolean" class="md-primary">Primary</md-switch>
     <HelloWorld msg="Welcome to MaterialsMine Project" />
   </div>
 </template>
@@ -11,8 +13,17 @@ import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
+  data: () => ({
+    boolean: true,
+  }),
   components: {
     HelloWorld,
   },
 };
 </script>
+
+<style lang="scss" scoped>
+  .md-switch {
+    display: flex;
+  }
+</style>
