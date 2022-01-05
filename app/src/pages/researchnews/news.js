c3be8c25572c5e8939e54bce585deb0307ef0fea
Rory Schadler <roryschadler@gmail.com>
2022-01-05 14:24:12 -0500

    #23: initial news page migration

--- app/src/pages/researchnews/news.js
+++ app/src/pages/researchnews/news.js
@@ -1,5 +1,10 @@
 export default {
   name: 'News',
+  methods: {
+    link (args) {
+      return window.open(args, '_blank');
+    }
+  },
   created () {
     this.$store.commit('setAppHeaderInfo', { icon: 'text_snippet', name: 'Research + News' })
   }
