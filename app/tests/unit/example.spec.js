8241b36252e4dcbecd2450f9f3ab93a3778ae420
tholulomo <fatexjames@yahoo.com>
2022-01-06 15:16:44 -0500

    #44: Sample testing review with @anyawallace

--- app/tests/unit/example.spec.js
+++ app/tests/unit/example.spec.js
@@ -1,12 +1,25 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
 
-describe('HelloWorld.vue', () => {
+
+describe('Header.vue', () => {
   it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const msg = 'Testing'
+    const wrapper = mount(HelloWorld, {
       propsData: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
+  })
+  it('add new item to movies', () => {
+    const msg = 'James Bond'
+    const wrapper = mount(HelloWorld)
+    const existingMovies = wrapper.vm.movies
+    wrapper.setData({movies: [...existingMovies, msg]})
+    expect(wrapper.vm.movies.length).toEqual(3)
+    expect(wrapper.vm.movies).toMatchSnapshot()
+  })
+  it('check h1 inside helloworld.vue', () => {
+    const wrapper = mount(HelloWorld)
+    expect(wrapper.html('h1').match('Testing'))
   })
 })
