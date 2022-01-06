0086470a509c1ed99c0d85d6df0a81d27050a12e
tholulomo <fatexjames@yahoo.com>
2022-01-06 15:15:38 -0500

    #44: Changed props for testing purpose

--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -38,7 +38,15 @@
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: {
+      type: String,
+      default: "Testing"
+    }
+  },
+  data: () => {
+    return {
+      movies: ['Test', 'move']
+    }
   }
 }
 </script>
