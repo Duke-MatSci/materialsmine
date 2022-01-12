085b38e39ea58b5bcd5b8def9b95bfd2b0074641
tholulomo <fatexjames@yahoo.com>
2022-01-12 10:37:34 -0500

    #62: Added a base vue component

---
+++ app/src/pages/explorer/basesample.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="page-container">
+		<md-app md-waterfall md-mode="fixed-last">
+			<md-app-toolbar :toggler='toggleMenu' />
+			<md-app-drawer :md-active.sync="menuVisible">
+				<drawer />
+			</md-app-drawer>
+			<md-app-content>
+				<!-- **** Content Goes Here *** -->
+			</md-app-content>
+		</md-app>
+	</div>
+</template>
+
+
+<script>
+import ExpHeader from '@/components/baseUI/explorer/Header.vue'
+import Drawer from '@/components/baseUI/explorer/Drawer.vue'
+export default {
+	name: 'Component Name',
+	components: {
+		mdAppToolbar: ExpHeader,
+		Drawer
+	},
+	data () {
+		return {
+			menuVisible: false
+		}
+	},
+	methods: {
+		toggleMenu () {
+		return this.menuVisible = !this.menuVisible
+		}
+  	},
+}
+</script>
