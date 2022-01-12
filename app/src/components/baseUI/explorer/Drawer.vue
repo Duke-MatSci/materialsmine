76cf1bf6dee588bcefd02ea481072f8df23f6768
tholulomo <fatexjames@yahoo.com>
2022-01-12 10:33:15 -0500

    #62: Created a header and drawer component

---
+++ app/src/components/baseUI/explorer/Drawer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <md-toolbar class="md-transparent" md-elevation="0">Welcome Stewart</md-toolbar>
+        <md-list>
+          <md-list-item>
+            <md-icon>send</md-icon>
+            <span class="md-list-item-text">Test</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>send</md-icon>
+            <span class="md-list-item-text">Test</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>send</md-icon>
+            <span class="md-list-item-text">Test</span>
+          </md-list-item>
+
+          <md-list-item>
+            <md-icon>send</md-icon>
+            <span class="md-list-item-text">Test</span>
+          </md-list-item>
+        </md-list>
+      </div>
+</template>
