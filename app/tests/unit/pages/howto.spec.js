f04592052db39ea66d321f82701917e05b5be581
Rory Schadler <roryschadler@gmail.com>
2022-01-14 11:30:48 -0500

    #23: Complete HowTo unit test
    HowTo tests were failing because v-playback had not been migrated, and
    howto.js was not managing videos correctly. That was fixed in a previous
    commit, this fleshes out the HowTo unit tests to account for the fixed
    functionality. The test has errors from video.js, which is used by
    v-playback. It seems like the testing harness is not providing valid
    DOM elements for display. Nonetheless, the tests pass, and when viewed,
    the page appears and loads videos just fine. I attempted to find the
    source of these errors, but wasn't able to.

--- app/tests/unit/pages/howto.spec.js
+++ app/tests/unit/pages/howto.spec.js
@@ -1,10 +1,14 @@
-import { shallowMount, createLocalVue } from '@vue/test-utils'
+import { mount, createLocalVue } from '@vue/test-utils'
+import Vue from 'vue'
 import Vuex from 'vuex'
+import vPlayBack from 'v-playback' 
+
 import HowTo from '@/pages/howTo/HowTo.vue'
 import store from '@/store'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
+localVue.use(vPlayBack)
 
 beforeAll(() => {
   jest.resetModules()
@@ -12,7 +16,47 @@ beforeAll(() => {
 
 describe('HowTo.vue', () => {
   it('mounts properly', () => {
-    const wrapper = shallowMount(HowTo, { store, localVue })
+    const wrapper = mount(HowTo, { store, localVue })
     expect(wrapper.exists()).toBeTruthy()
+  })
+
+  it('hides all videos to start', () => {
+    const wrapper = mount(HowTo, { store, localVue })
+    expect(wrapper.find('#videoPlayerContainer').exists()).toBeFalsy()
+  })
+
+  it('displays video on first click, hides it on second click', async () => {
+    // suppress jsdom alert 'Not implemented window.open'
+    window.open = jest.fn()
+    window.open.mockClear()
+
+    const wrapper = mount(HowTo, { store, localVue })
+    await Vue.nextTick() // wait for videos to load
+
+    const videoTitle = wrapper.find('.howto_item .howto_item-header')
+    expect(videoTitle.exists()).toBeTruthy()
+
+    await videoTitle.trigger('click')
+    expect(wrapper.find('#videoPlayerContainer').exists()).toBeTruthy()
+
+    await videoTitle.trigger('click')
+    expect(wrapper.find('#videoPlayerContainer').exists()).toBeFalsy()
+  })
+
+  it('closes any other open videos when a new one is clicked', async () => {
+    // suppress jsdom alert 'Not implemented window.open'
+    window.open = jest.fn()
+    window.open.mockClear()
+
+    const wrapper = mount(HowTo, { store, localVue })
+    await Vue.nextTick() // wait for videos to load
+
+    const videoTitle = wrapper.findAll('.howto_item .howto_item-header')
+
+    await videoTitle.at(0).trigger('click')
+    expect(wrapper.findAll('#videoPlayerContainer').length).toEqual(1)
+
+    await videoTitle.at(1).trigger('click')
+    expect(wrapper.findAll('#videoPlayerContainer').length).toEqual(1)
   })
 })
