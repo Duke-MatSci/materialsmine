4874064c0fd1f282f73b92fa7ccc8e8812aa7116
tholulomo <fatexjames@yahoo.com>
2022-01-18 00:46:33 -0500

    #62: Creating a sample usage after file arrangement refactor

--- app/src/pages/nanomine/Home/Home.vue
+++ app/src/pages/nanomine/Home/Home.vue
@@ -1,16 +1,29 @@
 <template>
-  <div class="home section_pages">
-    <div>Temporary NanoMine Home</div>
+  <div id="app" class="page-container">
+    <mdDrawer id="leftdrawer"></mdDrawer>
+    <page-header id="header" :info="info"></page-header>
+    <!-- <div>
+      Page Content Here
+    </div> -->
+    <page-footer id="footer"></page-footer>
   </div>
 </template>
 
 <script>
+import PageHeader from '@/components/nanomine/PageHeader.vue'
+import PageFooter from '@/components/nanomine/PageFooter.vue'
+import Drawer from '@/components/drawer.vue'
 
 export default {
   name: 'HomeNM',
   data: () => ({
     boolean: true
   }),
+  components: {
+    PageHeader,
+    PageFooter,
+    MdDrawer: Drawer
+  },
   created () {
     this.$store.commit('setAppHeaderInfo', {
       icon: '',
