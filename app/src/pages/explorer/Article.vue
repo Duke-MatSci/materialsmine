25b8d26745159d85581c208d75beaecf90d9ce76
Rory Schadler <roryschadler@gmail.com>
2022-01-20 14:37:21 -0500

    #66 initial article commit

---
+++ app/src/pages/explorer/Article.vue
@@ -0,0 +1,31 @@
+<template>
+  <md-app md-waterfall md-mode="fixed-last">
+    <md-app-toolbar :toggler='toggleMenu' />
+    <md-app-drawer :md-active.sync="toggleMenuVisibility">
+      <drawer />
+    </md-app-drawer>
+    <md-app-content>
+      <!-- **** Content Goes Here *** -->
+      <div class="article">
+        <h1>{{ doi }}</h1>
+      </div>
+    </md-app-content>
+  </md-app>
+</template>
+
+<script>
+export default {
+  name: 'Article',
+  props: {
+    doi: {
+      type: String,
+      default: '10.1002/polb.20925'
+    }
+  },
+  data: async (vm) => {
+    return {
+      SS_response: await (fetch(`https://api.semanticscholar.org/v1/paper/` + vm.doi)).json()
+    }
+  }
+}
+</script>
