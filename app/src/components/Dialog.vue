c865b5a40fa51e839e8b2eb5f97f823b97e96b1d
tholulomo <fatexjames@yahoo.com>
2022-01-25 05:01:30 -0500

    #68: Created dialog component and added dialog toggle to state

---
+++ app/src/components/Dialog.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <md-dialog-confirm
+      :md-active.sync="active"
+      :md-title="dialogTitle"
+      :md-content="textContent"
+      :md-confirm-text="confirmTextBtn"
+      :md-cancel-text="cancelTextBtn"
+      @md-cancel="onCancel"
+      @md-confirm="onConfirm" />
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Dialog',
+    props: {
+        active: {
+            type: Boolean,
+            default: false
+        },
+        cancelTextBtn: {
+            type: String,
+            default: 'Disagree'
+        },
+        confirmTextBtn: {
+            type: String,
+            default: 'Agree'
+        },
+        textContent: {
+            type: String,
+            default: `Welcome to <strong>Materialsmine</strong>.<br /> Here is a reusable dialog box in test mode`
+        },
+        dialogTitle: {
+            type: String,
+            default: `Dialog Box`
+        }
+    },
+    methods: {
+      onConfirm () {
+        this.value = 'Agreed'
+      },
+      onCancel () {
+        this.value = 'Disagreed'
+      }
+    }
+  }
+</script>
