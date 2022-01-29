379228162444c0190d30ebe9a4d218861b38d529
tholulomo <fatexjames@yahoo.com>
2022-01-29 16:03:09 -0500

    #83: unit test for metamine home page

---
+++ app/tests/unit/pages/metamine/home.spec.js
@@ -0,0 +1,25 @@
+import { mount } from '@vue/test-utils'
+import Home from '@/pages/metamine/Home.vue'
+import router from '@/router/index.js'
+
+const factory = () => {
+    return mount(Home, {
+        router,
+    })
+}
+
+describe('Nanomine Homepage', () => {
+  it('mount component correctly', async () => {
+    const wrapper = await factory()
+    expect(wrapper.find('.metamine_intro-header').text()).toBe('MetaMine')
+    expect(wrapper.find('.metamine_intro-span').text()).toBe('Material Informatics for MetaMaterials')
+    expect(wrapper.findAll('.card').length).toEqual(4)
+  })
+
+  it('run the openLink method', async () => {
+    const wrapper = await factory()
+    const link = wrapper.find('.card')
+    await link.trigger('click')
+    expect(wrapper.vm.$route.path).toEqual('/explorer')
+  })
+})
