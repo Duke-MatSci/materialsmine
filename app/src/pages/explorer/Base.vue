60779248486743775b69a7a299c41ed9fa1ec74c
tholulomo <fatexjames@yahoo.com>
2022-01-29 16:09:39 -0500

    #83: Re-usable base component for data explorer

---
+++ app/src/pages/explorer/Base.vue
@@ -0,0 +1,32 @@
+<template>
+	<md-app md-waterfall md-mode="fixed-last">
+		<md-app-toolbar :toggler='toggleMenu' />
+		<md-app-drawer :md-active.sync="toggleMenuVisibility">
+			<drawer />
+		</md-app-drawer>
+		<md-app-content>
+			<router-view />
+		</md-app-content>
+	</md-app>
+</template>
+<script>
+import ExpHeader from '@/components/explorer/Header.vue'
+import Drawer from '@/components/Drawer.vue'
+export default {
+  name: 'ExplorerBase',
+  components: {
+    mdAppToolbar: ExpHeader,
+    Drawer
+  },
+  data () {
+    return {
+      toggleMenuVisibility: false
+    }
+  },
+  methods: {
+    toggleMenu () {
+      this.toggleMenuVisibility = !this.toggleMenuVisibility
+    }
+  }
+}
+</script>
