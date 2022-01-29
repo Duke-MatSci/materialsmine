89416bdc2ca86993c42dff0f11c7aa00a7b24f9c
tholulomo <fatexjames@yahoo.com>
2022-01-29 16:18:13 -0500

    #83: Update to jest config specifically for mock images and docs

--- app/jest.config.js
+++ app/jest.config.js
@@ -5,5 +5,10 @@ module.exports = {
   },
   snapshotSerializers: [
     '<rootDir>/node_modules/jest-serializer-vue'
-  ]
+  ],
+  setupFiles: ['<rootDir>/tests/jest/script/test-setup.js'],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/tests/jest/__mocks__/fileMock.js',
+  },
 }
