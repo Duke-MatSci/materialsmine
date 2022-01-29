53277cca76856380759a32c5d230361f19c16af5
tholulomo <fatexjames@yahoo.com>
2022-01-29 16:08:21 -0500

    #83: Re-usable base component for nanomine pages

---
+++ app/src/pages/nanomine/Base.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="nanomine_app">
+    <mdDrawer id="leftdrawer"></mdDrawer>
+    <page-header id="header" :info="info"></page-header>
+    <router-view />
+    <page-footer id="footer"></page-footer>
+  </div>
+</template>
+
+<script>
+import PageHeader from '@/components/nanomine/PageHeader.vue'
+import PageFooter from '@/components/nanomine/PageFooter.vue'
+import Drawer from '@/components/nanomine/Drawer.vue'
+
+export default {
+  name: 'NanomineBase',
+  components: {
+    PageHeader,
+    PageFooter,
+    MdDrawer: Drawer
+  },
+}
+</script>
