f45f6a67a041dca424638ce3fd2c472a6fcb88ed
tholulomo <fatexjames@yahoo.com>
2022-01-29 16:03:41 -0500

    #83: Test for nanomine home page

---
+++ app/tests/unit/pages/nanomine/home.spec.js
@@ -0,0 +1,43 @@
+import { mount } from '@vue/test-utils'
+import Home from '@/pages/nanomine/home/Home.vue'
+import router from '@/router/index.js'
+
+const factory = (info = {}) => {
+    return mount(Home, {
+        router,
+        data() {
+            return {
+                gifChart: []
+            }
+        },
+        mocks: {
+            $store: {
+                commit: () => {
+                    return {
+                        setAppHeaderInfo: info
+                    }
+                }
+            }
+        }
+    })
+}
+
+describe('Nanomine Homepage', () => {
+  it('mount component correctly', async () => {
+    const wrapper = await factory()
+    await wrapper.setData({ gifChart: [{},{},{},{}] })
+
+    expect(wrapper.find('.visualize_header-h1').exists()).toBe(true)
+    expect(wrapper.findAll('.visualize_chart').length).toEqual(4)
+    expect(wrapper.find('.visualize_btn').exists()).toBe(true)
+    expect(wrapper.find('.section_quicklinks').exists()).toBe(true)
+    expect(wrapper.findAll('.quicklinks').length).toEqual(3)
+  })
+
+  it('navigates on quicklink clicks', async () => {
+    const wrapper = await factory()
+    const link = wrapper.find('.quicklinks')
+    await link.trigger('click')
+    expect(wrapper.vm.$route.path).toEqual('/xml-uploader')
+  })
+})
---
+++ app/tests/unit/pages/nanomine/howto.spec.js
@@ -0,0 +1,76 @@
+import { mount, createLocalVue } from '@vue/test-utils'
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+import HowTo from '@/pages/nanomine/howTo/HowTo.vue'
+import store from '@/store'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+beforeAll(() => {
+  jest.resetModules()
+})
+
+describe('HowTo.vue', () => {
+  it('mounts properly', () => {
+    const wrapper = mount(HowTo, { store, localVue })
+    expect(wrapper.exists()).toBeTruthy()
+  })
+
+  it('hides all videos to start', () => {
+    const wrapper = mount(HowTo, { store, localVue })
+    expect(wrapper.find('#videoPlayerContainer').exists()).toBeFalsy()
+  })
+
+  it('displays video on first click, hides it on second click', async () => {
+    // suppress jsdom alert 'Not implemented window.open'
+    window.open = jest.fn()
+    window.open.mockClear()
+
+    const wrapper = mount(HowTo, { store, localVue })
+    await Vue.nextTick() // wait for videos to load
+
+    const videoTitle = wrapper.find('.howto_item .howto_item-header')
+    expect(videoTitle.exists()).toBeTruthy()
+
+    await videoTitle.trigger('click')
+    expect(wrapper.find('video').exists()).toBeTruthy()
+
+    await videoTitle.trigger('click')
+    expect(wrapper.find('video').exists()).toBeFalsy()
+  })
+
+  it('closes any other open videos when a new one is clicked', async () => {
+    // suppress jsdom alert 'Not implemented window.open'
+    window.open = jest.fn()
+    window.open.mockClear()
+
+    const wrapper = mount(HowTo, { store, localVue })
+    await Vue.nextTick() // wait for videos to load
+
+    const videoTitle = wrapper.findAll('.howto_item .howto_item-header')
+
+    await videoTitle.at(0).trigger('click')
+    expect(wrapper.findAll('video').length).toEqual(1)
+
+    await videoTitle.at(1).trigger('click')
+    expect(wrapper.findAll('video').length).toEqual(1)
+  })
+
+  it('correctly opens a link if passed one', async () => {
+    // suppress jsdom alert 'Not implemented window.open'
+    window.open = jest.fn()
+    window.open.mockClear()
+
+    const wrapper = mount(HowTo, { store, localVue })
+    await Vue.nextTick() // wait for videos to load
+
+    const videoIcon = wrapper.find('.fa-youtube')
+
+    await videoIcon.trigger('click')
+
+    expect(window.open.mock.calls.length).toBeGreaterThanOrEqual(1)
+    expect(window.open.mock.calls[window.open.mock.calls.length - 1][0]).not.toBeNull()
+  })
+})
