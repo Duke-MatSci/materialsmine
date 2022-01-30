6a098222e38175c018459c4b4141b1a3baea7286
tholulomo <fatexjames@yahoo.com>
2022-01-30 02:10:43 -0500

    #83: Drawer rename causing errors

--- app/tests/unit/pages/nanomine/home.spec.js
+++ app/tests/unit/pages/nanomine/home.spec.js
@@ -1,5 +1,5 @@
 import { mount } from '@vue/test-utils'
-import Home from '@/pages/nanomine/home/Home.vue'
+import Home from '@/pages/nanomine/Home/Home.vue'
 import router from '@/router/index.js'
 
 const factory = (info = {}) => {
