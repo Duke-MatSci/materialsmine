c27e20837575ba97d6639a4d2f6544ea680a5586
tholulomo <94853475+tholulomo@users.noreply.github.com>
2022-01-30 02:20:04 -0500

    feat(#83): Nest routes based on category (#92)
    * #83: Mocks for jest testing
    
    * #83: unit test for metamine home page
    
    * #83: Test for nanomine home page
    
    * #83: Re-usable base component for nanomine pages
    
    * Re-usable base component for metamine pages
    
    * #83: Re-usable base component for data explorer
    
    * #83: Migrating metamine landing page, close #27
    
    * #83: Moved drawer file into component/nanomine dir
    
    * #83: Add image assets for nanomine and metamine landing pages
    
    * #83: Code refactor, partial contribution to #80
    
    * #83: Update to jest config specifically for mock images and docs
    
    * #83: Updates to style sheet for landing pages
    
    * #83: Refactor drawer for reusability - metamine & explorer
    
    * #83: Refactored Nanomine page header
    
    * #83: Refactor Nanomine page footer
    
    * #83: Refactor explorer page to use explorer base component
    
    * #83: Migrated Nanomine landing page, close #22
    
    * #83: Refactored router
    
    * #83: Nested routes based on category
    
    * #83: Updated test due to refactored codes
    
    * #83: Testing explorer landing page
    
    * #83: Linter error fixes
    
    * #83: jest config update
    
    * #83: Updates to drawer component for import
    
    * #83: Separate handlers in jest config and updated test
    
    * #83: Added additional handler to jest and updated file imports
    
    * #83: Updated file mock
    
    * #83: Linter error fixes
    
    * #83: Renamed file to avoid conflict
    
    * #83: Drawer rename causing errors
    
    * #83: Re added drawer file

---
+++ app/src/pages/metamine/Base.vue
@@ -0,0 +1,50 @@
+<template>
+    <md-app md-waterfall md-mode="fixed" id="metamine_app">
+        <md-app-toolbar class="md-large md-dense md-primary adjust_metamine-toolbar">
+            <div class="md-toolbar-row">
+                <div class="md-toolbar-section-start">
+                    <md-button class="md-icon-button" @click="toggleMenu">
+                        <md-icon class="metamine_menu-icon">menu</md-icon>
+                    </md-button>
+                    <span class="md-title adjust_metamine-title">Metamine</span>
+                </div>
+
+                <div class="md-toolbar-section-end">
+                    <md-button><span class="metamine_menu">ABOUT</span></md-button>
+                    <md-button href="/explorer"><span class="metamine_menu">VISUALIZE</span></md-button>
+                    <md-button><span class="metamine_menu">TOOLS</span></md-button>
+                    <md-button><span class="metamine_menu">CONTACT</span></md-button>
+                    <md-button>
+                        <span class="metamine_menu">LOGIN/REGISTER</span>
+                    </md-button>
+                </div>
+            </div>
+        </md-app-toolbar>
+        <md-app-drawer :md-active.sync="toggleMenuVisibility">
+            <drawers />
+        </md-app-drawer>
+        <md-app-content>
+			<router-view />
+		</md-app-content>
+    </md-app>
+</template>
+
+<script>
+import Drawers from '@/components/Drawer.vue'
+export default {
+  name: 'MetamineBase',
+  components: {
+    Drawers
+  },
+  data () {
+    return {
+      toggleMenuVisibility: false
+    }
+  },
+  methods: {
+    toggleMenu () {
+      this.toggleMenuVisibility = !this.toggleMenuVisibility
+    }
+  }
+}
+</script>
--- app/src/router/module/nanomine.js
+++ app/src/router/module/nanomine.js
@@ -1,54 +1,54 @@
 const nanomineRoutes = [
   {
-    path: '/nm',
+    path: '',
     name: 'HomeNM',
     component: () => import(/* webpackChunkName: "homenm" */ '@/pages/nanomine/Home/Home.vue'),
     meta: { requiresAuth: false }
   },
   {
-    path: '/nm/xml-uploader',
+    path: '/xml-uploader',
     name: 'XmlUploader',
     component: () => import(/* webpackChunkName: "xmlupload" */ '@/pages/nanomine/xmlUploader/XmlUploader.vue'),
     meta: { requiresAuth: false }
   },
   {
-    path: '/nm/modtools',
+    path: '/modtools',
     name: 'ModuleTools',
     component: () => import(/* webpackChunkName: "modtools" */ '@/pages/nanomine/tools/module/ModuleTools.vue'),
     meta: { requiresAuth: false }
   },
   {
-    path: '/nm/simtools',
+    path: '/simtools',
     name: 'SimulationTools',
     component: () => import(/* webpackChunkName: "simtools" */ '@/pages/nanomine/tools/simulation/SimulationTools.vue'),
     meta: { requiresAuth: false }
   },
   {
-    path: '/nm/plot-curation',
+    path: '/plot-curation',
     name: 'CsvPlotter',
     component: () => import(/* webpackChunkName: "plotcuration" */ '@/pages/nanomine/tools/csvPlotter/CsvPlotter.vue'),
     meta: { requiresAuth: false }
   },
   {
-    path: '/nm/chemprops',
+    path: '/chemprops',
     name: 'ChemProps',
     component: () => import(/* webpackChunkName: "chemprops" */ '@/pages/nanomine/tools/chemProps/ChemProps.vue'),
     meta: { requiresAuth: false }
   },
   {
-    path: '/nm/about',
+    path: '/about',
     name: 'About',
     component: () => import(/* webpackChunkName: "about" */ '@/pages/teams/Teams.vue'),
     meta: { requiresAuth: true }
   },
   {
-    path: '/nm/how',
+    path: '/how',
     name: 'HowTo',
     component: () => import(/* webpackChunkName: "howto" */ '@/pages/nanomine/howTo/HowTo.vue'),
     meta: { requiresAuth: false }
   },
   {
-    path: '/nm/news',
+    path: '/news',
     name: 'News',
     component: () => import(/* webpackChunkName: "news" */ '@/pages/nanomine/researchnews/News.vue'),
     meta: { requiresAuth: false }
