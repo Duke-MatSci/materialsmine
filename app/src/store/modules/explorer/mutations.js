77a2f480e8631348b9ed2f5ae12250ac971deaf8
tholulomo <fatexjames@yahoo.com>
2022-02-03 23:44:57 -0500

    #64: Updated global state with facet

--- app/src/store/modules/explorer/mutations.js
+++ app/src/store/modules/explorer/mutations.js
@@ -1,5 +1,15 @@
 export default {
   setMenuVisible (state) {
     state.toggleMenuVisibility = !state.toggleMenuVisibility
+  },
+  setSearchKeyword (state, payload) {
+    state.searchKeyword = payload
+  },
+  setSearching (state) {
+    if(!!state?.searchKeyword?.length){
+      state.searching = true
+    } else {
+      state.searching = false
+    }
   }
 }
