2f06dcbb0beadfdf301a046fb0a064e6634ce7c7
tholulomo <fatexjames@yahoo.com>
2022-02-03 23:42:01 -0500

    #64: Added src code for facet panel

---
+++ app/src/components/explorer/Facet.vue
@@ -0,0 +1,22 @@
+<template>
+    <div :class="{facet: true, active: searchEnabled}">
+        <div class="facet_viewport">
+            <md-list>
+                <md-subheader class="md-primary facet-header">Filter Panel</md-subheader>
+                <md-divider></md-divider>
+                <slot >Default Test</slot>
+            </md-list>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'FacetPanel',
+    props: ['searchEnabled']
+}
+</script>
+<style lang="scss" scoped>
+    .active {
+        opacity: .6;
+    }
+</style>
---
+++ app/src/modules/facet.js
@@ -0,0 +1,129 @@
+window.onload = function() {
+    initDragElement();
+    initResizeElement();
+  };
+  
+  function initDragElement() {
+    var pos1 = 0,
+      pos2 = 0,
+      pos3 = 0,
+      pos4 = 0;
+    var facets = document.getElementsByClassName("facet");
+    var elmnt = null;
+    var currentZIndex = 100; //TODO reset z index when a threshold is passed
+  
+    for (var i = 0; i < facets.length; i++) {
+      var facet = facets[i];
+      var header = getHeader(facet);
+  
+      facet.onmousedown = function() {
+        this.style.zIndex = "" + ++currentZIndex;
+      };
+  
+      if (header) {
+        header.parentFacet = facet;
+        header.onmousedown = dragMouseDown;
+      }
+    }
+  
+    function dragMouseDown(e) {
+      elmnt = this.parentFacet;
+      elmnt.style.zIndex = "" + ++currentZIndex;
+  
+      e = e || window.event;
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+  
+    function elementDrag(e) {
+      if (!elmnt) {
+        return;
+      }
+  
+      e = e || window.event;
+      // calculate the new cursor position:
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      // set the element's new position:
+      elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+      elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
+    }
+  
+    function closeDragElement() {
+      /* stop moving when mouse button is released:*/
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+  
+    function getHeader(element) {
+      var headerItems = element.getElementsByClassName("facet-header");
+  
+      if (headerItems.length === 1) {
+        return headerItems[0];
+      }
+  
+      return null;
+    }
+  }
+  
+  function initResizeElement() {
+    var facets = document.getElementsByClassName("facet");
+    var element = null;
+    var startX, startY, startWidth, startHeight;
+  
+    for (var i = 0; i < facets.length; i++) {
+      var p = facets[i];
+  
+      var right = document.createElement("div");
+      right.className = "resizer-right";
+      p.appendChild(right);
+      right.addEventListener("mousedown", initDrag, false);
+      right.parentFacet = p;
+  
+      var bottom = document.createElement("div");
+      bottom.className = "resizer-bottom";
+      p.appendChild(bottom);
+      bottom.addEventListener("mousedown", initDrag, false);
+      bottom.parentFacet = p;
+  
+      var both = document.createElement("div");
+      both.className = "resizer-both";
+      p.appendChild(both);
+      both.addEventListener("mousedown", initDrag, false);
+      both.parentFacet = p;
+    }
+  
+    function initDrag(e) {
+      element = this.parentFacet
+  
+      startX = e.clientX;
+      startY = e.clientY;
+      startWidth = parseInt(
+        document.defaultView.getComputedStyle(element).width,
+        10
+      );
+      startHeight = parseInt(
+        document.defaultView.getComputedStyle(element).height,
+        10
+      );
+      document.documentElement.addEventListener("mousemove", doDrag, false);
+      document.documentElement.addEventListener("mouseup", stopDrag, false);
+    }
+  
+    function doDrag(e) {
+      element.style.width = startWidth + e.clientX - startX + "px";
+      element.style.height = startHeight + e.clientY - startY + "px";
+    }
+  
+    function stopDrag() {
+      document.documentElement.removeEventListener("mousemove", doDrag, false);
+      document.documentElement.removeEventListener("mouseup", stopDrag, false);
+    }
+  }
+  
