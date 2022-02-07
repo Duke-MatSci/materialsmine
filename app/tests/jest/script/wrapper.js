f9a2425895d08f75749b23e375445ac5a5811fbb
tholulomo <fatexjames@yahoo.com>
2022-02-07 01:22:52 -0500

    #64: Created a re-usable wrapper for testing

---
+++ app/tests/jest/script/wrapper.js
@@ -0,0 +1,32 @@
+import _ from 'lodash'
+import VueMaterial from 'vue-material'
+import store from '@/store'
+import router from '@/router'
+import { mount, createLocalVue, shallowMount } from '@vue/test-utils'
+
+/** Add external plugins as needed */
+const localVue = createLocalVue()
+localVue.use(VueMaterial)
+
+/**
+ * createWrapper - creates a default mount wrapper for all components
+ * @param {*} component vue component to test
+ * @param {*} overrides props override
+ * @param {Boolean} useMount true for mount and false for shallowMount
+ * @returns {*} Vue Wrapper
+ */
+function createWrapper (component, overrides, useMount = true) {
+    const mountType = !useMount ? shallowMount : mount
+    const defaultMountOptions = {
+        localVue,
+        // mocks: {
+        //     $router: _.merge({ push: jest.fn() }, overrides.router || {})
+        // },
+        router,
+        store,
+        propsData: _.merge({}, overrides)
+    }
+    return mountType(component, defaultMountOptions)
+}
+
+export default createWrapper
