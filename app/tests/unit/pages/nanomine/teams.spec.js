c7b5bddbc9d6efa643a94e41b0758d2f294b1cb3
Rory Schadler <roryschadler@gmail.com>
2022-02-08 14:26:04 -0500

    #101: Refactor tests to use new createWrapper
    Refactored tests to remove repeated setup code and use new consistent
    `createWrapper`. Also suppressed `console.log` calls in howto tests

--- app/tests/unit/pages/nanomine/teams.spec.js
+++ app/tests/unit/pages/nanomine/teams.spec.js
@@ -1,25 +1,23 @@
-import { mount, createLocalVue } from '@vue/test-utils'
-import Vuex from 'vuex'
+import createWrapper from '../../../jest/script/wrapper'
 import TeamsPage from '@/pages/nanomine/teams/Teams.vue'
-import store from '@/store'
 
-const localVue = createLocalVue()
-localVue.use(Vuex)
+var wrapper = null
 
 describe('Teams.vue', () => {
+  beforeAll(() => {
+    wrapper = createWrapper(TeamsPage, { props: {}, slots: {} })
+  })
+
   it('mounts properly', () => {
-    const wrapper = mount(TeamsPage, { store, localVue })
     expect(wrapper.exists()).toBeTruthy()
   })
 
   it('loads team members', () => {
-    const wrapper = mount(TeamsPage, { store, localVue })
     const teamsList = wrapper.get('.teams_list').get('ul')
     expect(teamsList.findAll('.teams_container').length).toBeGreaterThanOrEqual(6)
   })
 
   it('loads partners', () => {
-    const wrapper = mount(TeamsPage, { store, localVue })
     const partnerContainer = wrapper.get('.team_partner-container')
     expect(partnerContainer.findAll('.teams_partner').length).toBeGreaterThanOrEqual(4)
   })
