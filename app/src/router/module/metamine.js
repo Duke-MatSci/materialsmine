3230833657702f2664649c7495d1031dc1e1a0d5
Rory Schadler <roryschadler@gmail.com>
2022-02-08 15:27:01 -0500

    #28: Link MM/teams to teams page

--- app/src/router/module/metamine.js
+++ app/src/router/module/metamine.js
@@ -4,6 +4,12 @@ const metamineRoutes = [
     name: 'HomeMM',
     component: () => import(/* webpackChunkName: "homemm" */ '@/pages/metamine/Home.vue'),
     meta: { requiresAuth: false }
+  },
+  {
+    path: 'teams',
+    name: 'Teams',
+    component: () => import(/* webpackChunkName: "about" */ '@/pages/nanomine/teams/Teams.vue'),
+    meta: { requiresAuth: false }
   }
 ]
 
