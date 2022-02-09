02e5336d3133fc7603e2b327f501d328c11c6e0d
Anya Wallace <aswallace98@gmail.com>
2022-02-09 15:23:09 -0800

    #104: Migrate dialog box styling from whyis, add named slots

--- app/src/components/Dialog.vue
+++ app/src/components/Dialog.vue
@@ -1,9 +1,26 @@
 <template>
   <div>
-    <md-dialog
+    <md-dialog class="dialog-box"
       :md-active.sync="active"
       :md-click-outside-to-close="false">
-      <slot></slot>
+      <div class="dialog-box_header">
+        <md-dialog-title>
+          <slot name="title">
+          </slot>
+        </md-dialog-title>
+      </div>
+      <div class="dialog-box_content">
+        <md-dialog-content>
+          <slot name="content">
+          </slot>
+        </md-dialog-content>
+      </div>
+      <div class="dialog-box_actions">
+        <md-dialog-actions>
+          <slot name="actions">
+          </slot>
+        </md-dialog-actions>
+      </div>
       </md-dialog>
   </div>
 </template>
