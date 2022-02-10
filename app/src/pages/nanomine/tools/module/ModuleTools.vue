0462d392915e8b2e939171a7d5541e69e6061fbc
Rory Schadler <roryschadler@gmail.com>
2022-02-10 15:47:46 -0500

    #25: initial ModuleTools commit
    Replaced vuetify elements with vue-material and simplified layout.

--- app/src/pages/nanomine/tools/module/ModuleTools.vue
+++ app/src/pages/nanomine/tools/module/ModuleTools.vue
@@ -1,2 +1,14 @@
 <template src="./moduleTools.html"></template>
 <script src="./moduleTools.js"></script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  img {
+    width: 240px;
+  }
+  h3 {
+    color: #096ff4;
+  }
+  h4 {
+    text-transform: uppercase;
+  }
+</style>
