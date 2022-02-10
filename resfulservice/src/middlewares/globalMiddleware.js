f415edef179dfad659bc9a7bf96116a385ba8a31
tholulomo <fatexjames@yahoo.com>
2022-02-10 15:33:16 -0500

    #85: Moved all global middleware services into a single file

---
+++ resfulservice/src/middlewares/globalMiddleware.js
@@ -0,0 +1,34 @@
+const bodyParser = require('body-parser');
+const acceptedHeaders = require('./accept');
+const getEnv = require('./parseEnv');
+const { fileMgr, fileServer } = require('./fileStorage');
+const mmGraphQL = require('../graphql');
+const { logParser, mmLogger } = require('./loggerService');
+
+const log = mmLogger();
+
+/**
+ * globalMiddleWare - this function that instantiate all 
+ * global middleware services for the REST API
+ * @param {*} app Express app object
+ */
+const globalMiddleWare = (app) => {
+    app.use(bodyParser.json());
+    app.use(fileMgr);
+    app.use('/mm_fils', fileServer);
+    app.use(acceptedHeaders);
+    app.use('/graphql', mmGraphQL);
+    app.use(getEnv);
+    app.use(
+        (req, res, next) => logParser(log, req, next)
+    );
+};
+
+/**
+ * This file imports all GLOBAL middleware services &
+ * exports them to the server.js file.
+ */
+module.exports = {
+    log,
+    globalMiddleWare
+};
