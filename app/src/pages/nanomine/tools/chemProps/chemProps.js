d2559643b2504afa646a5ced5432f68fa5c76d0d
Rory Schadler <roryschadler@gmail.com>
2022-02-11 15:36:03 -0500

    #25: Initial chemprops commit
    Rewrote chemProps.html to use `vue-material`, removed `Axios` and
    commented out Smiles.vue and Auth.js in chemProps.js. Still need to
    implement SMILES API, so that chemProps form works -- future ticket.

--- app/src/pages/nanomine/tools/chemProps/chemProps.js
+++ app/src/pages/nanomine/tools/chemProps/chemProps.js
@@ -1,5 +1,208 @@
+import {} from 'vuex'
+// import { Auth } from '@/modules/Auth.js'
+// import Smiles from './Smiles'
+const SERVER = `${window.location.origin}/nmr/api`
+// const SERVER = `http://localhost:8000/nmr/api`
+const URL = SERVER
+
 export default {
   name: 'ChemProps',
+  components: {
+    // Smiles
+  },
+  data () {
+    return {
+      title: 'ChemProps',
+      dialog: false,
+      pfRadios: 'pol',
+      quicksearchkeyword: '',
+      chemicalname: '',
+      abbreviation: '',
+      SMILES: '',
+      tradename: '',
+      stdname: '',
+      density: '',
+      uSMILES: '',
+      searchError: false,
+      searchErrorMsg: '',
+      loginRequired: false,
+      loginRequiredMsg: '',
+      successDlg: false,
+      theme: 'dark',
+      smilesError: false,
+      smilesMessage: '',
+      inputStr: '',
+      molecularFormula: '',
+      chempropsToken: null,
+      // https://github.com/reymond-group/smilesDrawer#options
+      smilesOptions: {
+        Padding: 0.0,
+        atomVisualization: 'default', // 'balls',
+        explicitHydrogens: true,
+        terminalCarbons: true,
+        debug: false
+      },
+      referenceOpen: false
+    }
+  },
+  watch: {
+    stdname (newData, oldData) {
+      if (newData) {
+        this.scrollToResult()
+      }
+    }
+  },
+  beforeMount: function () {
+    // this.auth = new Auth()
+  },
+  async mounted () {
+    let result = await fetch(`${URL}/parser`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    })
+    if (result && result.status === 201) {
+      result = await result.json()
+      if (result.token) this.chempropsToken = result.token
+    }
+  },
+  methods: {
+    scrollToResult () {
+      const elem = document.getElementById('chemprops-displayed-result')
+      if (elem) {
+        setTimeout(function () {
+          elem.scrollIntoView()
+        }, 800)
+      }
+    },
+    setLoading: function () {
+      this.$store.commit('isLoading')
+    },
+    resetLoading: function () {
+      this.$store.commit('notLoading')
+    },
+    resetOutput: function () {
+      this.stdname = ''
+      this.density = ''
+      this.searchError = false // reset the error message on click
+    },
+    successDlgClicked: function () {
+      // this.$router.go(-1) // go back to previous page
+      this.successDlg = false
+    },
+    search: function () {
+      this.resetOutput()
+      if (!this.chempropsToken) {
+        this.searchError = true
+        this.searchErrorMsg = 'System error, contact our system administrator'
+        return
+      }
+      if (this.pfRadios === 'pol' && this.quicksearchkeyword.trim() !== '') {
+        if (this.quicksearchkeyword === '') {
+          this.searchError = true
+          this.searchErrorMsg = 'Please input the quick search keyword.'
+          return
+        }
+        this.chemicalname = this.quicksearchkeyword
+        this.abbreviation = this.quicksearchkeyword
+        this.tradename = this.quicksearchkeyword
+        this.SMILES = this.quicksearchkeyword
+      }
+      if (this.chemicalname === '') {
+        this.searchError = true
+        this.searchErrorMsg = 'Please input the chemical name.'
+        return
+      }
+      if (this.pfRadios === '') {
+        this.searchError = true
+        this.searchErrorMsg = 'Please select the collection.'
+        return
+      }
+      // TODO need to configure after nmcp API done
+      this.setLoading()
+      // Axios.request({
+      //   url: `${URL}/chemprops?polfil=${this.pfRadios}&nmId=restNmId&chemicalname=${this.chemicalname}&abbreviation=${this.abbreviation}&tradename=${this.tradename}&smiles=${this.SMILES}`,
+      //   method: 'get',
+      //   headers: {
+      //     Accept: 'application/json',
+      //     'Content-Type': 'application/json',
+      //     Authorization: 'Bearer ' + this.chempropsToken
+      //   }
+      // })
+      fetch(new URL(`${URL}/chemprops?polfil=${this.pfRadios}&nmId=restNmId&chemicalname=${this.chemicalname}&abbreviation=${this.abbreviation}&tradename=${this.tradename}&smiles=${this.SMILES}`),
+        {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + this.chempropsToken
+          }
+        }
+      )
+        .then(function (res) {
+          return res
+        })
+        .then(function (response) {
+          this.stdname = response.data.data.StandardName
+          this.density = parseFloat(response.data.data.density)
+          // show uSMILES if it's polymer search
+          if (this.pfRadios === 'pol') {
+            this.uSMILES = response.data.data.uSMILES
+          }
+          // check if stdname is found
+          if (this.stdname === '') {
+            this.searchError = true
+            this.searchErrorMsg = 'No results found. Admin will update the database soon. Please try again in a week.'
+            this.resetOutput()
+          }
+          this.resetLoading()
+        })
+        .catch(function (error) {
+          console.log(error)
+          this.resetOutput()
+          this.searchError = true
+          if (error.message.includes('404')) {
+            this.searchErrorMsg = 'No results found. Admin will update the database soon. Please try again in a week.'
+          } else {
+            this.searchErrorMsg = 'An exception occurred when calling the ChemProps API service.'
+          }
+          this.resetLoading()
+        })
+        .then(function () {
+        // always executed
+          this.inputStr = this.uSMILES
+          // reset input if using quick search
+          if (this.quicksearchkeyword.trim() !== '') {
+            this.chemicalname = ''
+            this.abbreviation = ''
+            this.tradename = ''
+            this.SMILES = ''
+          }
+        })
+    },
+    onSuccess () {
+      this.smilesError = false
+    },
+    formulaUpdated (formula) {
+      this.molecularFormula = formula
+    },
+    onError (err) {
+      console.trace('Error handler called: ')
+      if (err) {
+        this.smilesMessage = err
+      } else {
+        this.smilesMessage = 'Undefined error'
+      }
+      this.smilesError = true
+      console.log('SmilesTest - error: ' + this.smilesMessage)
+    },
+    refOpen () {
+      this.referenceOpen = !this.referenceOpen
+      return this.referenceOpen
+    }
+  },
   created () {
     this.$store.commit('setAppHeaderInfo', { icon: 'workspaces', name: 'ChemProps' })
   }
---
+++ app/tests/unit/pages/nanomine/chemprops.spec.js
@@ -0,0 +1,26 @@
+import createWrapper from '../../../jest/script/wrapper'
+import ChemProps from '@/pages/nanomine/tools/chemProps/chemProps.vue'
+
+var wrapper = null
+global.console = {
+  log: jest.fn(), // console.log are ignored in tests
+
+  // Keep native behavior for other methods
+  error: console.error,
+  warn: console.warn,
+  info: console.info,
+  debug: console.debug
+}
+global.fetch = jest.fn()
+
+describe('ChemProps.vue', () => {
+  beforeAll(() => {
+    wrapper = createWrapper(ChemProps, {})
+  })
+
+  it('mounts properly', () => {
+    expect(wrapper.exists()).toBeTruthy()
+  })
+
+  // TODO write tests for form logic, once SMILES api is in place
+})
