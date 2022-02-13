7bb87bd276123136ba7ee9b41aaa7b7ca99dae9e
Rory Schadler <roryschadler@gmail.com>
2022-02-13 13:37:43 -0500

    #25: Added support for external links to ToolCard

--- app/src/components/nanomine/ToolCard.vue
+++ app/src/components/nanomine/ToolCard.vue
@@ -6,7 +6,8 @@
       </md-card-media>
       <md-card-header v-if="title" key="tool_title">
           <div class="md-title" v-if="link" key="has_tool_link">
-            <router-link :to="link">{{ title }}</router-link>
+            <router-link v-if="internalLink()" :to="link">{{ title }}</router-link>
+            <a v-else :href="link" target="_blank">{{ title }}</a>
           </div>
           <div class="md-title" v-else key="no_tool_link">
             {{ title }}
@@ -43,6 +44,23 @@ export default {
       type: Boolean,
       required: false,
       default: false
+    }
+  },
+  methods: {
+    internalLink: function () {
+      try {
+        const URLLink = new URL(this.link)
+        return URLLink.host === window.location.host
+      } catch (e) {
+        // URL() throws TypeError if its argument isn't a valid link --
+        // then it's probably an internal link, we can let router-link
+        // handle it
+        if (e instanceof TypeError) {
+          return true
+        } else {
+          throw e
+        }
+      }
     }
   }
 }
