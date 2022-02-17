aeffefe4a1250be46c663abd0d9bb73e71d4b75e
Anya Wallace <aswallace98@gmail.com>
2022-02-17 14:48:34 -0800

    #97: Remove test content from tabs

--- app/src/components/explorer/SearchHeader.vue
+++ app/src/components/explorer/SearchHeader.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="explorer_page_header">
+        <md-card style="padding:2rem; margin:2rem;">
         <div class="search_box_form">
             <div class="form__group search_box_form-item-1">
                 <input type="text" ref="search_input" class="form__input form__input--adjust" placeholder="Search" name="search" id="search" required v-model="searchWord" />
@@ -9,13 +10,15 @@
                 <button type="submit" class="btn btn--primary btn--noradius search_box_form_btn">Search</button>
             </div>
         </div>
+        <div style="text-align:right"> Query "{{searchWord}}" found 12345 results </div>
+        </md-card>
         <md-tabs class="btn--primary">
-            <md-tab :md-label="'Articles (' + articles + ')'"> Article Tab </md-tab>
-            <md-tab :md-label="'Samples (' + samples + ')'"> Sample Tab </md-tab>
-            <md-tab :md-label="'Images (' + images + ')'"> Image Tab </md-tab>
-            <md-tab :md-label="'Charts (' + charts + ')'"> Chart Tab </md-tab>
-            <md-tab :md-label="'Materials (' + materials + ')'"> Materials Tab </md-tab>
-            <md-tab :md-label="'Other (' + other + ')'"> Other Tab </md-tab>
+            <md-tab :md-label="'Articles (' + articles + ')'"></md-tab>
+            <md-tab :md-label="'Samples (' + samples + ')'"></md-tab>
+            <md-tab :md-label="'Images (' + images + ')'"></md-tab>
+            <md-tab :md-label="'Charts (' + charts + ')'"></md-tab>
+            <md-tab :md-label="'Materials (' + materials + ')'"></md-tab>
+            <!-- <md-tab :md-label="'Other (' + other + ')'"></md-tab> -->
         </md-tabs>
     </div>
 </template>
@@ -46,10 +49,10 @@ export default {
     },
     materials () {
       return 0
-    },
-    other () {
-      return 0
     }
+    // other () {
+    //   return 0
+    // }
   }
 }
 </script>
