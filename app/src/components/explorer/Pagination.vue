7e622fdb5244cf3938f7f423d8c6289ab6434694
Sam Stouffer <samuellstouffer@gmail.com>
2022-02-17 15:33:06 -0500

    remove redundant pagination styles

--- app/src/components/explorer/Pagination.vue
+++ app/src/components/explorer/Pagination.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="pagination-container">
+  <div class="viz-pagination-container">
     <span>Page {{ cpage }} of {{ tpages }}</span>
-    <div class="pagination">
+    <div class="viz-pagination">
       <button
         :disabled="cpage <= 1"
         @click.prevent="goToPage(1)"
@@ -44,69 +44,3 @@ export default {
   }
 }
 </script>
-<style lang="scss" scoped>
-@import "@/assets/css/abstract/_mixins.scss";
-@import "@/assets/css/abstract/_variables.scss";
-.pagination-container {
-  width: 80%;
-  margin: 1.5rem auto;
-  text-align: center;
-
-  @include respond(tab-land) {
-    max-width: 28%;
-    margin: 0 auto;
-    margin-top: 3.5rem;
-  }
-}
-
-.pagination {
-  margin-top: 0.8rem;
-  @include respond(tab-port) {
-    margin: 0 auto;
-    display: grid;
-    grid-template-rows: repeat(1, 100%);
-    grid-template-columns: repeat(4, 1fr);
-    grid-column-gap: 1rem;
-    margin: 1rem auto;
-    max-width: 70%;
-  }
-
-  @include respond(tab-land) {
-    max-width: 100%;
-  }
-
-  & > .pagination-button {
-    display: inline;
-    padding: 10px 24px !important;
-    color: $primary-white !important;
-    font-size: 0.8rem;
-    border: none;
-    background-color: $primary;
-    text-align: center;
-    cursor: pointer;
-    margin-right: 0.8rem;
-
-
-    @include respond(tab-land) {
-      padding: 10px !important;
-      font-size: 1rem;
-      margin: 0;
-    }
-
-    &:hover {
-      text-decoration: none;
-      border: none;
-      background-color: rgba($primary, 0.8);
-      color: $primary-white !important;
-    }
-
-    &[disabled="disabled"] {
-      background-color: $primary-grey;
-      cursor: default;
-    }
-  }
-  &-active {
-    background-color: rgba($primary, 0.8);
-  }
-}
-</style>
