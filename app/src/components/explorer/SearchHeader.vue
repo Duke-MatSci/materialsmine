49abdaaed082e9c43ecfb3a8ce3160e21e2bae3f
Anya Wallace <aswallace98@gmail.com>
2022-02-16 17:11:03 -0800

    #97: Initial commit of search header panel tabs

---
+++ app/src/components/explorer/SearchHeader.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="explorer_page_header">
+        <md-tabs class="btn--primary">
+            <md-tab :md-label="'Images (' + images + ')'"> Image Tab </md-tab>
+            <md-tab :md-label="'Samples (' + samples + ')'"> Sample Tab </md-tab>
+            <md-tab :md-label="'Articles (' + articles + ')'"> Article Tab </md-tab>
+            <md-tab :md-label="'Charts (' + charts + ')'"> Chart Tab </md-tab>
+            <!-- <md-tab :md-label="'Datasets (' + datasets + ')'"> Dataset Tab </md-tab>
+            <md-tab :md-label="'Other (' + other + ')'"> Other Tab </md-tab> -->
+        </md-tabs>
+    </div>
+</template>
+<script>
+export default {
+  name: 'SearchHeader',
+  props: ['searchEnabled'],
+  computed: {
+    images () {
+      return 0
+    },
+    samples () {
+      return 0
+    },
+    articles () {
+      return 0
+    },
+    charts () {
+      return 0
+    }
+    // datasets () {
+    //     return 0
+    // },
+    // other () {
+    //     return 0
+    // },
+  }
+}
+</script>
