25563b28649b88194596be1e248c37dd9e3e95e1
Anya Wallace <aswallace98@gmail.com>
2022-02-16 17:21:26 -0800

    #97: Add search bar to header

--- app/src/components/explorer/SearchHeader.vue
+++ app/src/components/explorer/SearchHeader.vue
@@ -1,5 +1,14 @@
 <template>
     <div class="explorer_page_header">
+        <div class="search_box_form">
+            <div class="form__group search_box_form-item-1">
+                <input type="text" ref="search_input" class="form__input form__input--adjust" placeholder="Search" name="search" id="search" required v-model="searchWord" />
+                <label htmlFor="search" class="form__label search_box_form_label">Search</label>
+            </div>
+            <div class="form__group search_box_form-item-2">
+                <button type="submit" class="btn btn--primary btn--noradius search_box_form_btn">Search</button>
+            </div>
+        </div>
         <md-tabs class="btn--primary">
             <md-tab :md-label="'Images (' + images + ')'"> Image Tab </md-tab>
             <md-tab :md-label="'Samples (' + samples + ')'"> Sample Tab </md-tab>
@@ -15,6 +24,14 @@ export default {
   name: 'SearchHeader',
   props: ['searchEnabled'],
   computed: {
+    searchWord: {
+      get () {
+        return this.$store.getters['explorer/getSearchKeyword']
+      },
+      set (payload) {
+        return this.$store.commit('explorer/setSearchKeyword', payload)
+      }
+    },
     images () {
       return 0
     },
