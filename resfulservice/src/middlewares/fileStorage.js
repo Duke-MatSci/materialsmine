dd19f07551cd09d023e17cfe9724162f52a9396d
tholulomo <fatexjames@yahoo.com>
2022-02-19 18:49:32 -0500

    #100: renamed filestorage directory and added a method to ensure unique file names

--- resfulservice/src/middlewares/fileStorage.js
+++ resfulservice/src/middlewares/fileStorage.js
@@ -1,13 +1,20 @@
 const path = require('path');
 const express = require('express');
 const multer = require('multer');
+const { uniqueNamesGenerator, adjectives, names, animals } = require('unique-names-generator')
+
+const shortName = uniqueNamesGenerator({
+  dictionaries: [adjectives, animals, names],
+  length: 3,
+  style: 'lowerCase'
+});
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'mm_fils');
   },
   filename: (req, file, cb) => {
-    cb(null, new Date().toISOString() + '-' + file.originalname);
+    cb(null, shortName + '-' + new Date().toISOString() + '-' + file.originalname);
   }
 });
 
@@ -25,7 +32,7 @@ const fileFilter = (req, file, cb) => {
 
 const fileMgr = multer({ storage: fileStorage, fileFilter: fileFilter }).fields([{ name: 'uploadfile', maxCount: 20 }]);
 
-const fileServer = express.static(path.join(__dirname, 'mm_files'));
+const fileServer = express.static(path.join(__dirname, 'filestore'));
 
 module.exports = {
   fileMgr,
