884bfd441e644a9066ab9455e48ec32f3b3a1507
Rory Schadler <roryschadler@gmail.com>
2022-03-11 15:22:59 -0500

    #135: Convert reconstruction tools

--- app/src/pages/nanomine/tools/TransferLearning.vue
+++ app/src/pages/nanomine/tools/TransferLearning.vue
@@ -1,5 +1,26 @@
 <template>
-  <tool-template :tool="tool" :card="card"></tool-template>
+  <tool-template card name="TransferLearning" :reference-list="references">
+    <!-- card-specific slots -->
+    <template #actions>
+      <a href="https://ideal.mech.northwestern.edu/research/software/download/" target="_blank">
+        <md-button class="md-raised md-primary md-raised">
+          Request Download
+        </md-button>
+      </a>
+    </template>
+    <template #title>
+      Transfer Learning
+    </template>
+    <template #content>
+      Transfer learning captures spatial correlations for microstructure reconstruction by leveraging the superior
+      capabilities of deep convolution networks. It incorporates an encoder-decoder process process and feature-match
+      optimization algorithm using the pre-trained VGG19 model. It can handle complex microstructures, and has the
+      potential of expediting the discovery of new materials. Please refer to the article by
+      <a href="https://doi.org/10.1038/s41598-018-31571-7" target="_blank">Li et. al.</a> for more details. The code
+      package may be requested for research purposes by clicking on the link below. You will asked to fill in a form and
+      we will email you following the form completion.
+    </template>
+  </tool-template>
 </template>
 
 <script>
@@ -18,22 +39,9 @@ export default {
   },
   data: function () {
     return {
-      tool: {
-        name: 'TransferLearning',
-        link: 'https://ideal.mech.northwestern.edu/research/software/download/',
-        externalLink: true,
-        linkText: 'Request Download',
-        title: 'Transfer Learning',
-        text: 'Transfer learning captures spatial correlations for microstructure reconstruction by leveraging the superior capabilities ' +
-      'of deep convolution networks. It incorporates an encoder-decoder process process and feature-match optimization algorithm using ' +
-      'the pre-trained VGG19 model. It can handle complex microstructures, and has the potential of expediting the discovery of new ' +
-      'materials. Please refer to the article by Li et. al. (see references) for more details. The code package may be requested for ' +
-      'research purposes by clicking on the link below. You will asked to fill in a form and we will email you following the form completion.',
-        display: true,
-        references: [
-          '10.1038/s41598-018-31571-7'
-        ]
-      }
+      references: [
+        '10.1038/s41598-018-31571-7'
+      ]
     }
   }
 }
