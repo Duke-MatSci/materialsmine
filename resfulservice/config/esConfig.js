2cdb89d34f484b7c38497bd5d1be7382f4220f75
tholulomo <fatexjames@yahoo.com>
2022-03-13 12:10:03 -0400

    #140: Add ES config payload

---
+++ resfulservice/config/esConfig.js
@@ -0,0 +1,28 @@
+/**
+ * Elastic search index config
+ */
+module.exports = config = { 
+    settings: { 
+        number_of_shards: 2, 
+        analysis: { 
+            filter: { 
+                autocomplete_filter: { 
+                    "type": "edge_ngram", 
+                    "min_gram": 3, 
+                    "max_gram": 20 
+                } 
+            }, 
+            analyzer: { 
+                autocomplete: { 
+                    type: "custom", 
+                    tokenizer: "standard", 
+                    filter: [ 
+                        "lowercase", 
+                        "autocomplete_filter" 
+                    ] 
+                } 
+            } 
+        } 
+    } 
+};
+
