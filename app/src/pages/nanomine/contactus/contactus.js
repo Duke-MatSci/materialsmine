7cf66d5b17e860f4a507c30a64c9b0e1c7e4c769
Kevin Zuniga Cuellar <kzunigac@uvm.edu>
2022-03-15 15:31:09 -0400

    #26: add validation and onSubmit methods

--- app/src/pages/nanomine/contactus/contactus.js
+++ app/src/pages/nanomine/contactus/contactus.js
@@ -1,18 +1,77 @@
 export default {
-  name: 'Contactus',
-  data: () => ({
-    msg: 'Contact the MaterialsMine Team',
-    helpText: 'Enter problem description',
-    textData: '',
-    contactType: 'comments',
-    confirmationDialog: false,
-    errorState: false,
-    errorMsg: 'xyz'
-  }),
+  name: 'Contact',
+  data () {
+    return {
+      name: null,
+      email: null,
+      contactType: null,
+      message: null,
+      errors: []
+    }
+  },
   methods: {
-    // showBox() {
-    //   console.log(this.filter);
-    // },
+    validateForm: function (e) {
+      this.errors = []
+      if (!this.name) {
+        this.errors.push('Name required')
+      }
+      if (!this.email) {
+        this.errors.push('Email required')
+      } else if (!this.validEmail(this.email)) {
+        this.errors.push('Valid email required.')
+      }
+
+      if (!this.contactType) {
+        this.errors.push('Contact type required')
+      }
+
+      if (!this.message || this.message.trim() === '') {
+        this.errors.push('Message required')
+      }
+
+      if (!this.errors.length) {
+        return true
+      }
+      e.preventDefault()
+    },
+    validEmail: function (email) {
+      const re =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return re.test(email)
+    },
+
+    resetForm: function () {
+      this.name = null
+      this.email = null
+      this.message = null
+      this.contactType = null
+      this.message = null
+      this.errors = []
+    },
+
+    onSubmit: async function (e) {
+      e.preventDefault()
+      this.validateForm(e)
+      if (this.errors.length) {
+        return
+      }
+      /*
+      await fetch("/nmr/contact", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          contactType: this.contactType,
+          contactText: this.message,
+        }),
+      }).catch((err) => {
+        console.log(err);
+      );
+      */
+
+      this.resetForm()
+    }
   },
   created () {
     this.$store.commit('setAppHeaderInfo', {
