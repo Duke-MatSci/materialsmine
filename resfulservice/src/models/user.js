8f4d9b4bcc2cbfb724eaa056e903abc788939976
tholulomo <fatexjames@yahoo.com>
2022-06-20 12:32:17 -0400

    #190: Add xmldata model for xmldata collection

--- resfulservice/src/models/user.js
+++ resfulservice/src/models/user.js
@@ -1,30 +1,38 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const usersSchema = new Schema(
-  {
-    alias: {
-      type: String
-    },
-    userid: {
-      type: String
-    },
-    givenName: {
-      type: String
-    },
-    surName: {
-      type: String
-    },
-    displayName: {
-      type: String
-    },
-    email: {
-      type: String
-    },
-    apiAccess: {
-      type: [String]
-    }
+const usersSchema = new Schema({
+  alias: {
+    type: String
+  },
+  givenName: {
+    type: String
+  },
+  surName: {
+    type: String
+  },
+  displayName: {
+    type: String
+  },
+  email: {
+    type: String,
+    unique: true,
+    index: true
+  },
+  roles: {
+    type: String,
+    required: true,
+    default: 'member'
+  },
+  apiAccess: {
+    type: Schema.Types.ObjectId,
+    ref: 'ApiAccess'
   }
-);
+});
+
+// usersSchema.path('email').validate(async (email) => {
+//   const emails = await mongoose.model('User').countDocuments({ email });
+//   return !emails;
+// }, 'email already exist');
 
 module.exports = mongoose.model('User', usersSchema);
