ea5d581deea9d4327c91a159840aec6c94aa09e0
tholulomo <fatexjames@yahoo.com>
2022-06-20 12:09:41 -0400

    190: Modified restful tests

--- resfulservice/spec/graphql/schema/user.spec.js
+++ resfulservice/spec/graphql/schema/user.spec.js
@@ -1,5 +1,5 @@
-const {expect} = require('chai');
-const graphQlSchema = require('../../../src/graphql/typeDefs');
+const { expect } = require('chai');
+const graphQlSchema = require('../../../src/graphql');
 
 describe('Schema Unit Tests:', function () {
   it('should have Query name as schema query type', async function () {
@@ -13,24 +13,23 @@ describe('Schema Unit Tests:', function () {
   });
 
   it('should have user as a RootQuery field', async function () {
-    const {user} = graphQlSchema.getQueryType().getFields();
+    const { user } = graphQlSchema.getQueryType().getFields();
     expect(user.name).to.equal('user');
     expect(user.type.toString()).to.equal('User!');
   });
 
-  it('should have _id,alias,userid,givenName,surName,displayName,email,apiAccess as fields for User schema', async function () {
+  it('should have _id,alias,givenName,surName,displayName,email,apiAccess as fields for User schema', async function () {
     const user = graphQlSchema.getType('User');
     const keys = Object.keys(user.getFields());
-    expect(keys).to.include.members(['_id', 'alias', 'userid', 'givenName', 'surName', 'displayName', 'email', 'apiAccess']);
+    expect(keys).to.include.members(['_id', 'alias', 'givenName', 'surName', 'displayName', 'email', 'apiAccess']);
   });
 
   it('should have correct datatypes for User schema', async function () {
     const user = graphQlSchema.getType('User');
-    const {_id, alias, userid, givenName, surName, displayName, email, apiAccess} = user.getFields();
+    const { _id, alias, givenName, surName, displayName, email, apiAccess } = user.getFields();
 
     expect(_id.type.toString()).to.equal('ID');
     expect(alias.type.toString()).to.equal('String');
-    expect(userid.type.toString()).to.equal('String');
     expect(givenName.type.toString()).to.equal('String');
     expect(surName.type.toString()).to.equal('String');
     expect(displayName.type.toString()).to.equal('String');
