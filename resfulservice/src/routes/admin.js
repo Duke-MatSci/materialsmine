ffb342a58d4ae30606e328709de5004f67fb58b1
tholulomo <fatexjames@yahoo.com>
2022-06-20 23:10:07 -0400

    #190: Renamed file

---
+++ resfulservice/src/routes/admin.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const AdminController = require('../controllers/adminController');
+const loginController = require('../controllers/loginController');
+const { getInternal } = require('../middlewares/isInternal');
+
+router
+  .route('/es/bulkinsert')
+  // .get(getInternal, AdminController.pingElasticSearch)
+  // .post(getInternal, AdminController.initializeElasticSearch);
+  .post(AdminController.bulkElasticSearchImport)
+  .put(AdminController.dataDump);
+
+router
+  .route('/es')
+  // .get(getInternal, AdminController.pingElasticSearch)
+  // .post(getInternal, AdminController.initializeElasticSearch);
+  .get(AdminController.pingElasticSearch)
+  .post(AdminController.initializeElasticSearch)
+  .put(getInternal, AdminController.loadElasticSearch);
+
+router.route('/login').post(loginController.login);
+module.exports = router;
