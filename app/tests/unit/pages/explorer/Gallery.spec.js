403ba4c7243d6bb4447aafacbc25b4a58505a5b4
tholulomo <fatexjames@yahoo.com>
2022-07-17 21:17:24 -0400

    #220: Update unit testing

--- app/tests/unit/pages/explorer/Gallery.spec.js
+++ app/tests/unit/pages/explorer/Gallery.spec.js
@@ -1,25 +1,36 @@
 import createWrapper from '../../../jest/script/wrapper'
-// import { enableAutoDestroy, RouterLinkStub } from '@vue/test-utils'
-import { enableAutoDestroy } from '@vue/test-utils'
+import { enableAutoDestroy, RouterLinkStub } from '@vue/test-utils'
 import ExplorerGallery from '@/pages/explorer/Gallery.vue'
-// import { chartUriPrefix, toChartId } from '@/modules/vega-chart'
+import { chartUriPrefix, toChartId } from '@/modules/vega-chart'
 
-describe('ExplorerHome.vue', () => {
-  const itemsPerPage = 1
-  const mockValues = {
-    data: [
-      {
+const itemsPerPage = 1;
+
+const mockValues = {
+  data: [
+    {
+      _source: {
         description: 'Filler particles may be treated with a particle surface treatment to improve dispersion within the matrix. Here, pie and donut charts serve as an abstract illustration of this concept alongside Glass Transition Temperature (Tg) data. Hover over a region in the pie or donut charts to see a tooltip with the Matrix, Filler, or Particle Surface Treatment. Click and drag in either scatter plot to define a selection window, then click and drag or scroll to adjust the selection window. To pan or zoom in a scatter plot, hold down Shift while dragging or scrolling.',
-        identifier: 'http://nanomine.org/viz/d0c4446ac8ddd0ed',
+        identifier: `http://nanomine.org/viz/d0c4446ac8ddd0ed${Math.random()}`,
         label: 'Kaleidoscopic Nanocomposites',
       }
-    ],
-    total: itemsPerPage
-  }
+    }
+  ],
+  total: itemsPerPage
+}
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve(mockValues),
+    statusText: 'OK',
+    status: 200
+  })
+);
+
+describe('Gallery.vue', () => {
   let wrapper
-  beforeEach(() => {
-    wrapper = createWrapper(ExplorerGallery, {}, true)
-    // mock vuex here
+  beforeEach(async() => {
+    fetch.mockClear();
+    wrapper = await createWrapper(ExplorerGallery, {}, true)
   })
 
   enableAutoDestroy(afterEach)
@@ -29,28 +40,30 @@ describe('ExplorerHome.vue', () => {
     expect(wrapper.findAll('.gallery-item').length).toBe(itemsPerPage)
   })
 
-  // it('shows number of results', () => {
-  //   expect.assertions(1)
-  //   expect(wrapper.find('.u_content__result').text()).toMatch(
-  //     /^About [1-9]\d* results/
-  //   )
-  // })
+  it('shows number of results', () => {
+    expect.assertions(1)
+    expect(wrapper.find('.u_content__result').text()).toMatch(
+      /[1-9]\d* result/
+    )
+  })
 
-  // it('provides links for each result', () => {
-  //   const items = wrapper.vm.items
-  //   expect.assertions(items.length)
-  //   for (const idx of items.keys()) {
-  //     const item = items[idx]
-  //     expect(
-  //       wrapper.findAllComponents(RouterLinkStub).at(idx).props().to
-  //     ).toEqual({
-  //       name: 'ChartView',
-  //       params: { chartId: toChartId(item.identifier) }
-  //     })
-  //   }
-  // })
+  it('provides links for each result', async () => {
+    const items = wrapper.vm.items
+    expect.assertions(items.length)
+    for (const idx of items.keys()) {
+      const item = items[idx]
+      expect(
+        await wrapper.findAllComponents(RouterLinkStub).at(idx).props().to
+      ).toEqual({
+        name: 'ChartView',
+        params: { chartId: toChartId(item.identifier) }
+      })
+    }
+  })
 
+  /* MOCK RETURNS 1 ITEM THIS NO LONGER APPLIES */
   // it('paginates gallery items', async () => {
+  //   debugger;
   //   const initialItemId = wrapper.vm.items[0].identifier
   //   await wrapper.find('.pagination-button-next').trigger('click')
   //   const newItemId = wrapper.vm.items[0].identifier
---
+++ app/tests/unit/pages/explorer/Image.spec.js
@@ -0,0 +1,77 @@
+import createWrapper from '../../../jest/script/wrapper'
+import { enableAutoDestroy, RouterLinkStub } from '@vue/test-utils'
+import ImageGallery from '@/pages/explorer/image/Image.vue'
+
+
+let apollo = {
+  "images": {
+    "totalItems": 1,
+    "pageSize": 1,
+    "pageNumber": 1,
+    "totalPages": 1,
+    "hasPreviousPage": false,
+    "hasNextPage": true,
+    "images": [
+      {
+        "file": "/api/files/583e05cfe74a1d205f4e2177",
+        "description": "height image",
+        "microscopyType": "AFM",
+        "type": "grayscale",
+        "dimension": {
+          "width": 20,
+          "height": 20
+        },
+        "metaData": {
+          "title": "Morphology and electrical resistivity of melt mixed blends of polyethylene and carbon nanotube filled polycarbonate",
+          "doi": "10.1016/j.polymer.2003.10.003",
+          "id": "583e0672e74a1d205f4e21be",
+          "keywords": [
+            "Blends",
+            "Multiwalled carbon nanotube composites",
+            "Electrical resistivity"
+          ]
+        }
+      }
+    ]
+  }
+}
+describe('Image.vue', () => {
+  let wrapper
+  beforeEach(async() => {
+    wrapper = await createWrapper(ImageGallery, { mocks: {
+      $apollo: {
+        loading: false,
+      },
+    }}, true)
+    await wrapper.setData({ images: apollo.images })
+  })
+
+  enableAutoDestroy(afterEach)
+
+  // TODO: TEST 
+  it('loads page', async () => {
+    expect.assertions(1)
+    expect(await wrapper.find('.gallery').exists()).toBe(true)
+  })
+
+  it('shows number of results', async () => {
+    expect.assertions(2)
+    expect(await wrapper.find('.utility-roverflow').exists()).toBe(true)
+    expect(await wrapper.find('.u_content__result').text()).toMatch(
+      /[1-9]\d* result/
+    )
+  })
+
+  it('provides links for each result', async () => {
+    const images = wrapper.vm.images.images
+    expect.assertions(images.length)
+    for (const [idx, image] of images.entries()) {
+      expect(
+        await wrapper.findAllComponents(RouterLinkStub).at(idx).props().to
+      ).toEqual({
+        name: 'ImageDetailView',
+        params: { id: image.metaData.id, fileId: image.file }
+      })
+    }
+  })
+})
