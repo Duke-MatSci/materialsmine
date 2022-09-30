a3044c35cd742cde3a6d423a93265868be06963c
tholulomo <fatexjames@yahoo.com>
2022-09-30 18:50:09 -0400

    #252: Linter fixes

--- resfulservice/src/controllers/pixelatedController.js
+++ resfulservice/src/controllers/pixelatedController.js
@@ -5,8 +5,8 @@ const deleteFile = require('../utils/fileManager');
 exports.uploadPixelData = async (req, res, next) => {
   try {
     req.logger.info('pixelData Upload Function Entry:');
-    if (!req?.files?.uploadfile) return res.status(400).json({ message: 'PixelData csv not uploaded', statusCode: 400 });
-    if (!req?.userId) {
+    if (!req.files?.uploadfile) return res.status(400).json({ message: 'PixelData csv not uploaded', statusCode: 400 });
+    if (!req.userId) {
       req.logger?.error('[uploadPixelData]: User not authenticated to upload data');
       deleteFile(req.files.uploadfile[0].path, req);
       return res.status(401).json({ message: 'Not authenticated', statusCode: 401 });
@@ -25,7 +25,7 @@ exports.uploadPixelData = async (req, res, next) => {
 exports.updatePixelData = async (req, res, next) => {
   try {
     req.logger.info('pixelData Update Function Entry:');
-    if (!req?.userId) {
+    if (!req.userId) {
       req.logger?.error('[updatePixelData]: User not authenticated to update data');
       deleteFile(req.files.uploadfile[0].path, req);
       return res.status(401).json({ message: 'Not authenticated', statusCode: 401 });
