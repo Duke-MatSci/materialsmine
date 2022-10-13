b10006622239820f79612811faddec38c7a63717
tholulomo <94853475+tholulomo@users.noreply.github.com>
2022-10-13 14:18:17 -0400

    Fix (#252): Updates (#263)
    * #252: Updated graphql schema description
    
    * #252: Add changes to automatically create storage at RESTAPI start up

--- resfulservice/src/routes/pixelated.js
+++ resfulservice/src/routes/pixelated.js
@@ -2,9 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const pixelatedController = require('../controllers/pixelatedController');
-// const isAuth = require('../middlewares/isAuth');
+const isAuth = require('../middlewares/isAuth');
 
-router.route('/').post(pixelatedController.uploadPixelData);
-router.route('/edit').post(pixelatedController.updatePixelData);
+router.route('/').post(isAuth, pixelatedController.uploadPixelData);
+router.route('/edit').post(isAuth, pixelatedController.updatePixelData);
 
 module.exports = router;
