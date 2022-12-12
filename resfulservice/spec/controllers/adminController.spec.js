386b3d6dbf0695e95707012f8b37c553b4355eea
tholulomo <fatexjames@yahoo.com>
2022-12-12 10:48:17 -0500

    #207: Backend unit testing for admin controller apis

---
+++ resfulservice/spec/controllers/adminController.spec.js
@@ -0,0 +1,97 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const DatasetProperty = require('../../src/models/datasetProperty');
+const sinonChai = require('sinon-chai');
+const { getDatasetProperties } = require('../../src/controllers/adminController');
+
+chai.use(sinonChai);
+
+const { expect } = chai;
+
+const mockDatasetProperties =  [
+  {
+    attribute: 'http://materialsmine.org/ns/RealPermittivity',
+    label: 'Real Permittivity'
+  },
+  {
+    attribute: 'http://materialsmine.org/ns/RealRelativePermittivity',
+    label: 'Real Relative Permittivity'
+  },
+  {
+    attribute: 'http://materialsmine.org/ns/DielectricRealPermittivity',
+    label: 'Dielectric Real Permittivity'
+  },
+  {
+    attribute: 'http://materialsmine.org/ns/RealImpedance',
+    label: 'Real Impedance'
+  },
+  {
+    attribute: 'http://materialsmine.org/ns/RealPartOfConductivity',
+    label: 'Real part of Conductivity'
+  },
+  {
+    attribute: 'http://materialsmine.org/ns/RealPartOfPermittivity',
+    label: 'Real part of Permittivity'
+  }
+]
+
+const fetchedDatasetProperties = {
+  select: (properties) => mockDatasetProperties
+}
+
+describe('Admin Controllers Unit Tests:', function() {
+
+  afterEach(() => sinon.restore());
+
+  this.timeout(10000)
+
+  const req = { logger: { info: (_message) => { }, error: (_message) => { } } }
+
+  const res = {
+    status: () => {},
+    json: () => {},
+  };
+
+
+
+  context('getDatasetProperties', () => {
+    it('should return a 400 error if no search query params', async function() {
+      const next = function (fn) {
+        return fn;
+      };
+      sinon.stub(res, 'status').returnsThis();
+      sinon.stub(res, 'json').returnsThis();
+      const result = await getDatasetProperties(req, res, next);
+
+      expect(result).to.have.property('message');
+      expect(result.message).to.equal("search query params is missing");
+    });
+
+    it('should return a list of filtered dataset properties', async function() {
+      req.query = { search: 'Loss' }
+      const next = function (fn) {
+        return fn;
+      };
+      sinon.stub(res, 'status').returnsThis();
+      sinon.stub(res, 'json').returns({data: mockDatasetProperties});
+      sinon.stub(DatasetProperty, 'find').returns(fetchedDatasetProperties)
+      const result = await getDatasetProperties(req, res, next);
+
+      expect(result).to.have.property('data');
+    });
+
+    it('should return a 500 server error', async function() {
+      req.query = { search: 'Loss' }
+      const next = function (fn) {
+        return fn;
+      };
+      sinon.stub(res, 'status').returnsThis();
+      sinon.stub(res, 'json').returns({message: 'Server Error'});
+      sinon.stub(DatasetProperty, 'find').throws('Error connecting to database');
+
+      const result = await getDatasetProperties(req, res, next);
+      expect(result).to.have.property('message');
+      expect(result.message).to.equal('Error connecting to database');
+    });
+  })
+})
