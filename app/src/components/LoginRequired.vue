1b8bd179c330ae4d582c94cb8f7980d2326f3c6d
tolujay <tolulomo@toluconsulting.com>
2022-12-28 10:07:55 -0500

    Upstream update

---
+++ app/src/components/LoginRequired.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="page-not-found">
+        <i class="material-icons page-not-found-header u--padding-zero" >lock_person</i>
+        <h2 class="u_margin-bottom-small">Login required</h2>
+        <p>The page you are trying to access requires authentication.
+            Would you like to <a>log in</a> or return to the
+            <router-link to=""><a @click="$router.go(-1)">previous page</a></router-link>?</p>
+    </div>
+</template>
