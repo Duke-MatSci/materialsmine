2b4f8102d07ea690c050e653121c73a63ce8476c
tholulomo <fatexjames@yahoo.com>
2023-01-25 15:17:18 -0500

    #293: Fix mobile bug in viz page

--- app/src/pages/explorer/Visualization.vue
+++ app/src/pages/explorer/Visualization.vue
@@ -3,35 +3,31 @@
 		<div class="section_teams">
 			<div class="search_box">
 				<h2 class="search_box_header u_margin-bottom-big adjust_metamine-title">Gallery Categories:</h2>
-            </div>
-            <div class="curate">
-                <div class="md-layout md-gutter utility_flex_mobile">
-                    <div class="md-layout-item md-small-hide">&nbsp;</div>
-                    <div class="md-layout-item">
-                        <router-link to="/explorer/chart" v-slot="{navigate, href}" custom>
-                            <div class="teams_container explorer_page-nav-card md-layout-item_card" :href="href" @click="navigate">
-                                <md-icon class="explorer_page-nav-card_icon">ssid_chart</md-icon>
-                                <span class="explorer_page-nav-card_text">Charts</span>
-                                <p class="md-layout-item_para">Our chart builder leverages SPARQL and Vega-Lite for rich,
-                                    interactive charts of data from the MaterialsMine knowledge graph. Visit our Charts Gallery to view examples,
-                                    and contact us to get your data integrated.
-                                </p>
-                            </div>
-                        </router-link>
-                    </div>
-                    <div class="md-layout-item">
-                        <router-link to="/explorer/images" v-slot="{navigate, href}" custom>
-                            <div class="teams_container explorer_page-nav-card md-layout-item_card" :href="href" @click="navigate">
-                                <md-icon class="explorer_page-nav-card_icon">grid_view</md-icon>
-                                <span class="explorer_page-nav-card_text">Image</span>
-                                <p class="md-layout-item_para">Visit our Image Gallery to view microstructural images for nanocomposite systems stored
-                                    in the MaterialsMine knowledge graph, and filter by material constituents, nanoparticle geometry, and more.
-                                </p>
-                            </div>
-                        </router-link>
-                    </div>
-                    <div class="md-layout-item md-small-hide">&nbsp;</div>
-                </div>
+        <div class="md-layout md-layout-responsive">
+          <div class="md-layout-item md-layout-item_card">
+            <router-link to="/explorer/chart" v-slot="{navigate, href}" custom>
+              <div class="teams_container explorer_page-nav-card md-layout-item_card" :href="href" @click="navigate">
+                <md-icon class="explorer_page-nav-card_icon">ssid_chart</md-icon>
+                <span class="explorer_page-nav-card_text">Charts</span>
+                <p class="md-layout-item_para">Our chart builder leverages SPARQL and Vega-Lite for rich,
+                  interactive charts of data from the MaterialsMine knowledge graph. Visit our Charts Gallery to view examples,
+                  and contact us to get your data integrated.
+                </p>
+              </div>
+            </router-link>
+          </div>
+          <div class="md-layout-item md-layout-item_card">
+            <router-link to="/explorer/images" v-slot="{navigate, href}" custom>
+              <div class="teams_container explorer_page-nav-card md-layout-item_card" :href="href" @click="navigate">
+                <md-icon class="explorer_page-nav-card_icon">grid_view</md-icon>
+                <span class="explorer_page-nav-card_text">Image</span>
+                <p class="md-layout-item_para">Visit our Image Gallery to view microstructural images for nanocomposite systems stored
+                  in the MaterialsMine knowledge graph, and filter by material constituents, nanoparticle geometry, and more.
+                </p>
+              </div>
+            </router-link>
+          </div>
+        </div>
 			</div>
 		</div>
 	</div>
