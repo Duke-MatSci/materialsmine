10b533b3ad425eabcae34238917e4ca00e16b7e7
tholulomo <fatexjames@yahoo.com>
2023-02-08 02:41:09 -0500

    #9: Remove sample dev templates

--- services/app/errors/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import request, Blueprint
-
-errors = Blueprint("errors", __name__)
-
-@errors.route('/404')
-def notFoundError(opts: dict = {message: 'Page Not Found', desc: ''}):
-    return "{}, {}".format(opts.message, opts.desc)
--- services/app/posts/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
-
-
-class CreateNewPost(FlaskForm):
-    title = StringField("Post Title", validators=[DataRequired(), Length(min=2, max=120)])
-    content = TextAreaField("Content", validators=[DataRequired(), Length(min=2, max=520)])
-    submit = SubmitField("Submit")
