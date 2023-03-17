293ada75c7aac936f51c3faef4d95857f4ef7d1e
Anya Wallace <aswallace98@gmail.com>
2023-03-17 11:30:58 -0700

    fix(backend): refactor update and delete mutations for single mongodb call

--- resfulservice/src/graphql/resolver/material_template/mutation.js
+++ resfulservice/src/graphql/resolver/material_template/mutation.js
@@ -23,30 +23,31 @@ const materialMutation = {
     }
     const { field } = input;
     try {
-      const columnExists = await MaterialTemplate.findOne({ field });
-      if (!columnExists) return errorFormater('column not found', 404);
       const column = await MaterialTemplate.findOneAndUpdate({ field }, { $set: { ...input, user: user._id } }, { new: true, lean: true, populate: { path: 'user', select: 'displayName' } });
+      if (!column) return errorFormater('column not found', 404);
+      req.logger?.info(`[updateMaterialColumn]: column successfully updated: ${column.field}`);
       return { ...column, user: column.user.displayName };
     } catch (error) {
+      req.logger?.error(`[updateMaterialColumn]: ${error}`);
       return errorFormater(error.message, 500);
     }
   },
 
   deleteXlsxCurationList: async (_, { input }, { user, req, isAuthenticated }) => {
-    req.logger.info('deleteMaterialColumn Function Entry:');
+    req.logger.info('[deleteMaterialColumn] Function Entry:');
     if (!isAuthenticated) {
       req.logger?.error('[deleteMaterialColumn]: User not authenticated to remove listing');
       return errorFormater('not authenticated', 401);
     }
     const { field } = input;
     try {
-      const columnExists = await MaterialTemplate.findOne({ field });
-      req.logger?.info('columnExists: ');
-      req.logger?.info(columnExists);
-      if (!columnExists) return errorFormater('column not found', 404);
-      return MaterialTemplate.findOneAndDelete({ field }).lean();
+      const column = await MaterialTemplate.findOneAndDelete({ field }).lean();
+      if (!column) return errorFormater('column not found', 404);
+      req.logger?.info(`[deleteMaterialColumn]: column successfully deleted: ${column.field}`);
+      return column;
     } catch (error) {
-      return errorFormater(error.message, 400);
+      req.logger?.error(`[deleteMaterialColumn]: ${error}`);
+      return errorFormater(error, 400);
     }
   }
 };
