3dc7d95f0595d5cbe4db1b0580a495af3f1fbc63
Anya Wallace <aswallace98@gmail.com>
2023-05-26 13:25:25 -0700

    fix(#398): add Edge to supported browsers, update auth reroute

--- resfulservice/config/constant.js
+++ resfulservice/config/constant.js
@@ -5,7 +5,7 @@ module.exports = {
   images: 'about?view=instances&uri=http://semanticscience.org/resource/Image',
   charts: 'about?view=instances&uri=http://semanticscience.org/resource/Chart',
   sparql: 'sparql',
-  supportedBrowser: ['Firefox', 'Chrome', 'Canary', 'Safari', 'Opera', 'IE'],
+  supportedBrowser: ['Firefox', 'Chrome', 'Canary', 'Safari', 'Opera', 'IE', 'Edge'],
   userRoles: {
     isAdmin: 'isAdmin',
     member: 'member'
