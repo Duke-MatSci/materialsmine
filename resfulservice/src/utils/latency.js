98a7fd3d4426103e977539f494336e668ad90c83
tholulomo <fatexjames@yahoo.com>
2023-07-11 09:58:34 -0400

    feat(#416): Remove latency logic and merge into middleware

--- resfulservice/src/utils/latency.js
+++
@@ -1,7 +0,0 @@
-exports.latencyCalculator = (res) => {
-  const startTime = res.get('entrytime');
-  const endTime1 = new Date().getTime();
-  const latency = Math.floor((endTime1 - startTime));
-  res.header('endTime', endTime1);
-  res.header('latency', `${Math.floor((latency / 1000) % 60)} seconds`);
-};
