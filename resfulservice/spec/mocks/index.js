55f9a13a9f481053542eb2b0319c09256a25bfd4
tholulomo <fatexjames@yahoo.com>
2023-07-11 10:04:17 -0400

    feat(#416): Updating latency response middleware service

--- resfulservice/spec/mocks/index.js
+++ resfulservice/spec/mocks/index.js
@@ -1,7 +1,13 @@
 const curation = require('./curationMock');
 const user = require('./userMock');
 
+// It is re-useable across all tests files
+const next = function (fn) {
+  return fn;
+};
+
 module.exports = {
   ...curation,
-  ...user
+  ...user,
+  next
 };
