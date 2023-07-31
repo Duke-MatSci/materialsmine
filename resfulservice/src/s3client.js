f3122f4ffcfb32b68bd37b882aa758e16a469933
Cynthia2019 <ccynthia2019@gmail.com>
2023-07-30 18:47:29 -0700

    fix(restful): hide AWS key

--- resfulservice/src/s3client.js
+++ resfulservice/src/s3client.js
@@ -1,20 +1,12 @@
 
 const { S3Client } = require('@aws-sdk/client-s3');
 
-// const s3Client = new S3Client({
-//   region: process.env.AWS_S3_REGION || 'us-east-2',
-//   credentials: {
-//     accessKeyId: process.env.AWS_S3_ACCESS_KEY,
-//     secretAccessKey: process.env.AWS_S3_SECRET_KEY,
-//   },
-// });
-
 const s3Client = new S3Client({
-  region: 'us-east-2',
+  region: process.env.AWS_S3_REGION || 'us-east-2',
   credentials: {
-    accessKeyId: 'AKIAZCLM5UPUQ6TWICHO',
-    secretAccessKey: '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'
-  }
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_S3_SECRET_KEY,
+  },
 });
 
 module.exports = s3Client;
