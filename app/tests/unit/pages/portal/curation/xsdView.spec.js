e15caa9e6c55c5a2e970e81020d83f3ec402f791
tholulomo <fatexjames@yahoo.com>
2023-09-06 11:45:14 -0400

    feat(xml_query_fix): Adding unit test for xsd viewer

---
+++ app/tests/unit/pages/portal/curation/xsdView.spec.js
@@ -0,0 +1,36 @@
+import createWrapper from '../../../../jest/script/wrapper'
+import XsdView from '@/pages/portal/curation/xsd/XsdViewer.vue'
+
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({}),
+    statusText: 'OK',
+    status: 200
+  })
+)
+
+describe('XsdView.vue', () => {
+  let wrapper
+  beforeEach(async () => {
+    wrapper = await createWrapper(XsdView, { }, false)
+  })
+
+  afterEach(() => {
+    wrapper.destroy()
+    fetch.mockClear()
+  })
+
+  it('page mounts properly', () => {
+    expect.assertions(2)
+    expect(wrapper.exists()).toBeTruthy()
+    expect(fetch).toHaveBeenCalled()
+  })
+
+  it('renders components properly', async () => {
+    expect.assertions(2)
+    wrapper.setData({ xsd: null })
+    expect(wrapper.findComponent({ name: 'spinner' }).exists()).toBeTruthy()
+    await wrapper.setData({ xsd: 'any value' })
+    expect(wrapper.findComponent({ name: 'spinner' }).exists()).toBe(false)
+  })
+})
