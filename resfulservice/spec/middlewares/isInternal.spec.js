e81660defc116ed4a27a16ee4143f8c950987149
tholulomo <fatexjames@yahoo.com>
2023-10-07 13:05:28 -0400

    feat(#378): jwt service middleware refactoring

--- resfulservice/spec/middlewares/isInternal.spec.js
+++ resfulservice/spec/middlewares/isInternal.spec.js
@@ -1,25 +1,69 @@
-const { expect, should } = require('chai');
-const { setInternal, getInternal } = require('../../src/middlewares/isInternal');
+const { expect } = require('chai');
+const sinon = require('sinon');
 const { logger } = require('../common/utils');
+const { mockUser, next } = require('../mocks');
+const jwtService = require('../../src/utils/jwtService')
+const { setInternal, getInternal } = require('../../src/middlewares/isInternal');
 
-should();
+describe('isInternal Middleware service', function () {
+  afterEach(() => sinon.restore());
 
-describe('isInternal middleware service', function() {
-  it.skip('rejects req.isInternal undefined values', function() {
-    expect(getInternal.bind(this, {logger}, {}, () => {})).to.throw('Not authorized.');
-  });
+  const req = { 
+    logger,
+    get: () => {}
+  }
 
-  it.skip('authorizes request', async function() {
-    let req = {
-      logger,
-      isInternal: undefined,
-      internal: false,
-      get: () => ({}),
-    };
-    setInternal(req, {}, () => {});
-    req.isInternal = req.signedToken;
-    req.signedToken.should.exist;
-    getInternal(req, {}, () => {});
-    req.internal.should.equal(true);
-  });
+  const res = {
+    header: () => {},
+    status: () => {},
+    json: () => {},
+    send: () => {}
+  };
+
+  context('getInternal', () => {
+    it('rejects unauthorized request when no authorization header', async function () {
+      sinon.stub(req, 'get').returns(null)
+      try {
+        getInternal(req, res, next);
+      } catch (error) {
+        expect(error.message).to.equal('Not authorized.');
+      }
+    });
+  
+    it('rejects unauthorized request when decoding token throws error', async function () {
+      sinon.stub(req, 'get').returns("Bearer i2od2nlw4aeiavi2q3")
+      sinon.stub(jwtService, 'decodeToken').throws('JsonWebTokenError: jwt malformed');
+      try {
+        getInternal(req, res, next);
+      } catch (error) {
+        expect(error.message).to.equals('JsonWebTokenError: jwt malformed');
+      }
+    });
+  
+    it('authorizes request', async function () {
+      sinon.stub(req, 'get').returns("Bearer i2od2nlw4aeiavi2q3")
+      const  nextSpy = sinon.spy();
+      sinon.stub(jwtService, 'decodeToken').returns(mockUser);
+      getInternal(req, res, nextSpy);
+      sinon.assert.calledOnce(nextSpy);
+    });
+  })
+
+  context('setInternal', () => {
+    
+    it('rejects unauthorized request when decoding token throws error', async function () {
+      sinon.stub(jwtService, 'signToken').throws('Error');
+      try {
+        setInternal(req, res, next);
+      } catch (error) {
+        expect(error.message).to.equals('Error');
+      }
+    });
+  
+    it('authorizes request', async function () {
+      sinon.stub(jwtService, 'signToken').returns({ token: 'adia4kla49qnpove'});
+      const result = setInternal(req, res, next);
+      expect(result).to.have.property('token');
+    });
+  })
 });
