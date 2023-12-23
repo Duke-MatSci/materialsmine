7b85bae17b5c7b3d5badc9ab3b5e269f2db1fa67
tholulomo <fatexjames@yahoo.com>
2023-12-22 20:36:29 -0500

    #9: Fix latency issues for metamine viz

--- app/src/pages/metamine/visualizationNU/ScatterPlot.vue
+++ app/src/pages/metamine/visualizationNU/ScatterPlot.vue
@@ -8,7 +8,7 @@
     <template #main_chart>
       <Scatter />
       <div class="tools-simulation u--layout-flex u--layout-flex-justify-sb">
-        <!-- <button
+        <button
           @click="toggleEnableKnn()"
           :class="{
             'nuplot-button': enableKnn,
@@ -16,11 +16,8 @@
           }"
         >
           Find Nearest Neighbors
-        </button> -->
-        <md-switch v-model="enableKnn" @change="toggleEnableKnn()"
-          >Find Nearest Neighbors</md-switch
-        >
-        <dialog-box :active="dialogBoxActiveKnn">
+        </button>
+        <dialog-box disableClose :active="dialogBoxActiveKnn">
           <template v-slot:content> <NeighborPanel /> </template>
           <template v-slot:actions>
             <md-button @click.native.prevent="closeDialogBox">Close</md-button>
