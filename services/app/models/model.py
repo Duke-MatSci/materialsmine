f4558901144b13d2af25104d0ac3a7f94e9cf4b1
tholulomo <fatexjames@yahoo.com>
2024-01-23 02:31:41 -0500

    #461: removing thread lock on db

--- services/app/models/model.py
+++ services/app/models/model.py
@@ -1,137 +1,43 @@
 import urllib.parse
 import pymongo
 from app.models.constant import CHEMPROPS_COLLECTION
-import threading
 
+# Grouping Database related functions and instantiation
 class Database_Handler:
     def __init__(self, config):
-        """
-        Initializes the class with the specified configuration.
+        # Get config value
+        self.db_user = urllib.parse.quote_plus(config.MONGO_USER)
+        self.db_pass = urllib.parse.quote_plus(config.MONGO_PASSWORD)
+        self.db_name = config.MONGO_DATABASE
+        self.db_address = '{}:{}'.format(config.MONGO_URI, config.MONGO_PORT)
+        self.mgs_user = config.MGS_USER
+        self.mgs_pwd = config.MGS_PWD
 
-        Parameters:
-            config (object): The configuration object.
+        self.client = pymongo.MongoClient("mongodb://{}:{}@{}/{}?authSource=admin".format(self.db_user, self.db_pass, self.db_address, self.db_name))
+        existing_databases = self.client.list_database_names()
 
-        Returns:
-            None
-        """
-        self.config = config
-        self.database_lock = threading.Lock()
-        self._initialize_database_with_lock()
-        # try:
-        #     self._initialize_database()
-        # except Exception as e:
-        #     print(f"Error initializing database: {e}")
-            
-    def _initialize_database_with_lock(self):
-        """
-        Initializes the database with a lock.
-
-        This function is responsible for initializing the database.
-        It acquires a lock to ensure that only one thread can 
-        perform the initialization at a time. If the database has already
-        been initialized, this function does nothing.
-
-        Parameters:
-            self (object): The instance of the class.
-
-        Returns:
-            None
-        """
-        with self.database_lock:
-            if not hasattr(self, '_database_initialized') or not self._database_initialized:
-                db_thread = threading.Thread(target=self._initialize_database_thread)
-                db_thread.start()
-                db_thread.join()  # Optionally wait for the thread to finish
-                self._database_initialized = True
-
-    def _initialize_database_thread(self):
-        """
-        This method will be run in a separate thread.
-        """
-        try:
-            self._initialize_database()
-        except Exception as e:
-            print(f"Error initializing database in thread: {e}")
-            return None
-
-    def _initialize_database(self):
-        """
-        Initializes the database connection and sets up the
-        necessary database and collections.
-
-        Parameters:
-            self (obj): The current instance of the class.
-        
-        Returns:
-            None
-        """
-        connection_string = self._build_connection_string()
-        self.client = pymongo.MongoClient(connection_string)
-        self.mgs_database = self.client[self.config.MONGO_DATABASE]
-        print("self.mgs_database: ", self.mgs_database)
-
-        if self.config.MONGO_DATABASE not in self.client.list_database_names():
-            self._create_managed_services_database()
+        if self.db_name in existing_databases:
+            self.client = pymongo.MongoClient("mongodb://{}:{}@{}/{}".format(self.mgs_user, self.mgs_pwd, self.db_address, self.db_name))
         else:
-            print("Connected to MongoDB, database exists")
-
-
-    def _build_connection_string(self):
-        """
-        Builds a connection string for MongoDB using the provided
-        configuration.
-
-        Returns:
-            str: The MongoDB connection string.
-
-        """
-        return "mongodb://{}:{}@{}:{}/{}?authSource=admin".format(
-            urllib.parse.quote_plus(self.config.MONGO_USER),
-            urllib.parse.quote_plus(self.config.MONGO_PASSWORD),
-            self.config.MONGO_URI,
-            self.config.MONGO_PORT,
-            self.config.MONGO_DATABASE
-        )
-
-    def _create_managed_services_database(self):
-        """
-        Create and initialize the managed services database.
-
-        This function connects to MongoDB and creates the database
-        specified in the configuration.
-        It then inserts a test document into the `chemprops` collection
-        to ensure the database is functioning correctly.
-
-        Parameters:
-            self (object): The instance of the class.
+            try:
+                print("Managed services database does not exist, creating...")
+                self.client = pymongo.MongoClient("mongodb://{}:{}@{}/{}?authSource=admin".format(self.db_user, self.db_pass, self.db_address, self.db_name))
+                
+                # Create user for managed services DB
+                self.client[self.db_name].command('createUser', self.mgs_user,  pwd=self.mgs_pwd, roles=[{'role': 'readWrite', 'db': self.db_name}])
+            except pymongo.errors.OperationFailure:
+                # Notice: This is not essential, but this type of failure occurs if the db user exist
+                self.client = pymongo.MongoClient("mongodb://{}:{}@{}/{}".format(self.mgs_user, self.mgs_pwd, self.db_address, self.db_name))
         
-        Returns:
-            None
-        """
-        try:
-            self.mgs_database = self.client[self.config.MONGO_DATABASE]
-            self.chemprops_collection().insert_one({'test': 'data'})
-            print("Connected to MongoDB, database created")
-        except Exception as e:
-            print(f"Error creating managed services database: {e}")
-            exit(1)
-
+        # pass created database in mgs_database
+        self.mgs_database = self.client[self.db_name]
 
     def init_app(self, app):
-        """
-        Initializes the application by setting the database attribute
-        of the app object to the MongoDB database.
-
-        :param app: The Flask application object.
-        :type app: Flask
-        :return: The initialized Flask application object.
-        :rtype: Flask
-        """
         app.db = self.mgs_database
         return app
 
+    # TODO (BINGYIN): Remove if not needed. This is an example 
+    # to show that we can connect and return a specific collection
     def chemprops_collection(self):
-        """
-        Returns the `chemprops_collection` attribute of the class instance.
-        """
+        # TODO (@BINGYIN): provide the table name here. Change name from constant.py!
         return self.mgs_database[CHEMPROPS_COLLECTION]
