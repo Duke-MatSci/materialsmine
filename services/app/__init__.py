3d24ad9cbb36450cd6ced42c3c1e039426cf4038
tholulomo <fatexjames@yahoo.com>
2024-01-23 07:47:41 -0500

    #461: Refactoring database connection

--- services/app/__init__.py
+++ services/app/__init__.py
@@ -7,19 +7,18 @@ from app.config import Config
 from app.models.model import Database_Handler
 import logging
 
-# Connect Database
-db = Database_Handler(Config)
-
 # TODO (@Tee): Enable the ones we need
 # bcrypt = Bcrypt()
 # mail = Mail()
 
 def create_app(config_class = Config):
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(config_class)
     # bcrypt.init_app(app)
     # mail.init_app(app)
-    db.init_app(app)
+
+    # Initialize database handler
+    app.db = Database_Handler(config_class)
     CORS(app)
     
     # Configure logging
@@ -30,19 +29,19 @@ def create_app(config_class = Config):
     file_handler.setLevel(logging.INFO)  # Set log level for the file handler
     app.logger.addHandler(file_handler) 
 
-    from app.chemprops.routes import chemprops
-    from app.intellicharact.routes import icharact
-    from app.otsu.routes import otsu
-    from app.descriptor.routes import descriptor
-    from app.correlation.routes import correlation
+    # from app.chemprops.routes import chemprops
+    # from app.intellicharact.routes import icharact
+    # from app.otsu.routes import otsu
+    # from app.descriptor.routes import descriptor
+    # from app.correlation.routes import correlation
     from app.dynamfit.routes import dynamfit
     from app.main.routes import main
 
-    app.register_blueprint(chemprops)
-    app.register_blueprint(icharact)
-    app.register_blueprint(otsu)
-    app.register_blueprint(descriptor)
-    app.register_blueprint(correlation)
+    # app.register_blueprint(chemprops)
+    # app.register_blueprint(icharact)
+    # app.register_blueprint(otsu)
+    # app.register_blueprint(descriptor)
+    # app.register_blueprint(correlation)
     app.register_blueprint(dynamfit)
     app.register_blueprint(main)
 
--- services/app/models/model.py
+++ services/app/models/model.py
@@ -1,102 +1,22 @@
-import urllib.parse
 import pymongo
-from flask import current_app as app
-from app.models.constant import CHEMPROPS_COLLECTION
 
 class Database_Handler:
     def __init__(self, config):
-        """
-        Initializes the class with the specified configuration.
-
-        Parameters:
-            config (object): The configuration object.
-
-        Returns:
-            None
-        """
         self.config = config
-        try:
-            self._initialize_database()
-        except Exception as e:
-            print(f"Error initializing database: {e}")
-            
-
-    def _initialize_database(self):
-        """
-        Initializes the database connection and sets up the
-        necessary database and collections.
-
-        Parameters:
-            self (obj): The current instance of the class.
-        
-        Returns:
-            None
-        """
-        connection_string = self._build_connection_string()
-        self.client = pymongo.MongoClient(connection_string)
-        self.mgs_database = self.client[self.config.MONGO_DATABASE]
-
-        if self.config.MONGO_DATABASE not in self.client.list_database_names():
-            self._create_managed_services_database()
-        else:
-            print("Connected to MongoDB, database exists")
-
+        self.client = pymongo.MongoClient(self._build_connection_string())
+        self.database = self._get_or_create_database()
 
     def _build_connection_string(self):
-        """
-        Builds a connection string for MongoDB using the provided
-        configuration.
+        return f"mongodb://{self.config.MONGO_USER}:{self.config.MONGO_PASSWORD}" \
+               f"@{self.config.MONGO_URI}:{self.config.MONGO_PORT}/{self.config.MONGO_DATABASE}"
 
-        Returns:
-            str: The MongoDB connection string.
-
-        """
-        return "mongodb://{}:{}@{}:{}/{}?authSource=admin".format(
-            urllib.parse.quote_plus(self.config.MONGO_USER),
-            urllib.parse.quote_plus(self.config.MONGO_PASSWORD),
-            self.config.MONGO_URI,
-            self.config.MONGO_PORT,
-            self.config.MONGO_DATABASE
-        )
-
-    def _create_managed_services_database(self):
-        """
-        Create and initialize the managed services database.
-
-        This function connects to MongoDB and creates the database
-        specified in the configuration.
-        It then inserts a test document into the `chemprops` collection
-        to ensure the database is functioning correctly.
-
-        Parameters:
-            self (object): The instance of the class.
-        
-        Returns:
-            None
-        """
-        try:
-            self.client[self.config.MONGO_DATABASE]
-            self.chemprops_collection().insert_one({'test': 'data'})
-            print("Connected to MongoDB, database created")
-        except Exception as e:
-            print(f"Error creating managed services database: {e}")
-
-
-    def init_app(self, app):
-        """
-        Initializes the application by setting the database attribute
-        of the app object to the MongoDB database.
-
-        :param app: The Flask application object.
-        :type app: Flask
-        :return: The initialized Flask application object.
-        :rtype: Flask
-        """
-        app.db = self.mgs_database
-        return app
-
-    def chemprops_collection(self):
-        """
-        Returns the `chemprops_collection` attribute of the class instance.
-        """
-        return self.mgs_database[CHEMPROPS_COLLECTION]
+    def _get_or_create_database(self):
+        database = self.client[self.config.MONGO_DATABASE]
+        if self.config.MONGO_DATABASE not in self.client.list_database_names():
+            # Create the database and any required collections or initial data here
+            try:
+                self.chemprops_collection().insert_one({'test': 'data'})
+                print("Connected to MongoDB, collection created")
+            except Exception as e:
+                print(f"Error creating managed services database: {e}")
+        return database
