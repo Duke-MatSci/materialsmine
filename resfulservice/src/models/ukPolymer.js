8a2d17b9b2292b21b6207300f12d32059ad1c2e4
tholulomo <fatexjames@yahoo.com>
2024-05-21 10:58:41 -0400

    chores: Adding unknown polymer collections for chemprops managed services

---
+++ resfulservice/src/models/ukPolymer.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+const polymerSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true
+  },
+  _stdname: {
+    type: String,
+    required: true
+  },
+  _abbreviations: {
+    type: [String],
+    default: []
+  },
+  _synonyms: {
+    type: [String],
+    default: []
+  },
+  _tradenames: {
+    type: [String],
+    default: []
+  },
+  _density: {
+    type: Number
+  },
+  _boc: {
+    type: [String],
+    default: []
+  }
+});
+
+module.exports = mongoose.model('ukpolymer', polymerSchema);
