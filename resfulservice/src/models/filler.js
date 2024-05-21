6c6e902cc912b09abb6917c67cc46764978e94ac
tholulomo <fatexjames@yahoo.com>
2024-05-21 10:55:07 -0400

    chores: Adding filler collections for chemprops managed services

---
+++ resfulservice/src/models/filler.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const fillerSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true
+  },
+  _density: {
+    type: Number
+  },
+  _alias: {
+    type: [String],
+    default: []
+  },
+  _boc: {
+    type: [String],
+    default: []
+  }
+});
+
+module.exports = mongoose.model('filler', fillerSchema);
