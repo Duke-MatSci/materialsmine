1f34f0cc4afea9879706dba44c98f2ae135b472c
tholulomo <fatexjames@yahoo.com>
2024-05-21 11:00:01 -0400

    chores: Adding a database handler route for chemprops

--- resfulservice/src/routes/managed-service.js
+++ resfulservice/src/routes/managed-service.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const manageServiceController = require('../controllers/managedServiceController');
 const isAuth = require('../middlewares/isAuth');
+const { managedServiceDBCall } = require('../middlewares/validations');
 const { latencyTimer } = require('../middlewares/latencyTimer');
 
 router
@@ -11,5 +12,9 @@ router
 router
   .route('/:appName')
   .post(isAuth, latencyTimer, manageServiceController.manageServiceRequest);
+
+router
+  .route('/db/:collectionName')
+  .post(managedServiceDBCall, latencyTimer, manageServiceController.dbRequest);
 
 module.exports = router;
