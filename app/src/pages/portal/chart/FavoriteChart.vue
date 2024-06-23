b543911078da468be646f8b4833790a415b02117
tholulomo <fatexjames@yahoo.com>
2024-06-23 19:37:02 -0400

    feat(#493): Creating favourite chart pages for users and administrators

---
+++ app/src/pages/portal/chart/FavoriteChart.vue
@@ -0,0 +1,18 @@
+<template>
+  <chart-gallery sender="Favourite Chart" :isFavourite="isAuth" />
+</template>
+<script>
+import { mapGetters } from 'vuex';
+import ChartGallery from '@/components/ChartGallery.vue';
+export default {
+  name: 'FavouriteChart',
+  components: {
+    ChartGallery
+  },
+  computed: {
+    ...mapGetters({
+      isAuth: 'auth/isAuthenticated'
+    })
+  }
+};
+</script>
