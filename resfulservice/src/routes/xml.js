5923059670862791c8769b1eed40da044032ec6e
tholulomo <fatexjames@yahoo.com>
2025-03-13 03:52:08 -0400

    FEAT(#461): Adding a new xml has property route

--- resfulservice/src/routes/xml.js
+++ resfulservice/src/routes/xml.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const { latencyTimer } = require('../middlewares/latencyTimer');
 const curationController = require('../controllers/curationController');
+const isAuth = require('../middlewares/isAuth');
 const router = express.Router();
 
 router
   .route('/:controlID')
   .get(latencyTimer, curationController.getCurationXml);
+
+router
+  .route('/xml-has-property')
+  .post(isAuth, latencyTimer, curationController.loadXmlTable);
 
 module.exports = router;
